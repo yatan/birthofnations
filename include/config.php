cc11bf7320c5f5de584f78f4792de96ecf66cf62
yatan87@gmail.com <yatan@users.noreply.github.com>
2011-06-24 15:19:28 +0000

    estructura carpetas

---
+++ include/config.php
@@ -0,0 +1,15 @@
+<?
+
+
+$server="localhost"; /* Nuestro server mysql */
+$database=""; /* Nuestra base de datos */
+$dbpass=""; /*Nuestro password mysql */
+$dbuser=""; /* Nuestro user mysql */
+
+
+$link=mysql_connect($server,$dbuser,$dbpass);
+
+mysql_select_db($database, $link);
+
+?>
+
