2b282ceb99a86e93f77a13cd13a5755185a22eed
kojie90@gmail.com <kojie90@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2011-06-25 19:10:29 +0000

    babas

--- index.php
+++ index.php
@@ -1,27 +1,12 @@
-<?
-
-include_once("include/funciones.php");
-
-?>
-<!--
-To change this template, choose Tools | Templates
-and open the template in the editor.
--->
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<!DOCTYPE html>
 <html>
-  <head>
-    <title></title>
-	<link type="text/css" href="css/css.css" rel="Stylesheet" /> 
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  </head>
-  <body>
-      <center>
-          <img src="images/titulo.png" alt="logo"/>
-      </center>
-    TODO write content
-	
-	<div id="pie"> 
-		<center>Estado mysql: <? online(); ?></center> 
-	</div> 
-  </body>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
 </html>
