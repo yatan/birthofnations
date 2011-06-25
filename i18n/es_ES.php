660429dc489bab4a772f7f5b77a1a9b57c568ff2
Etchner@gmail.com <Etchner@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2011-06-25 22:12:02 +0000

    Adding new text from the signup form (registro.php)

--- i18n/es_ES.php
+++ i18n/es_ES.php
@@ -1,6 +1,6 @@
 <?php
 
-$dicc = array (
+$mail_activation = array (
     'activacion_titulo' => 'Activación de su cuenta Birth of Nations',
     'activacion_mensaje_titulo' => "Bienvenido a Birth of Nations, ",
     'activacion_mensaje_cuerpo' => "pero antes es necesario que actives la cuenta para poder jugar.
@@ -8,6 +8,17 @@ $dicc = array (
     'activacion_mensaje_nick' => "Usuario registrado: ",    
     'activacion_mensaje_mensaje1' => "Si no te has registrado en Birth of Nations, simplemente ignora el correo.",
     'activacion_mensaje_firma' => "Atte. El equipo de BirthofNations",    
-
     );
+
+
+$signup_form = array (
+    'signup_title' => "Formulario de Registro"    ,
+    'signup_username' => "Nombre de usuario "    ,
+    'signup_username_info' => "Caracteres de A-z, mínimo 5 caracteres "    ,
+    'signup_password' => "Mínimo 5 caracteres, máximo 12 caracteres, letras y "    ,
+    'signup_password2' => "Debe ser igual a la anterior "    ,
+    'signup_mail' => "email"    ,
+    'signup_mail_info' => 'Escribe un email válido por favor',
+    'signup_submit' => "Formulario de Registro"    ,
+);
 ?>
