5d93dccaf313875a6169624e7c3b29ee50e4ece6
Etchner@gmail.com <Etchner@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2011-06-26 21:40:55 +0000

    en catalan, gracias Eligius ;)

---
+++ trunk/i18n/ca_ES.php
@@ -0,0 +1,35 @@
+    <?php
+
+    $mail_activation = array (
+        'activacion_titulo' => 'Activació del seu compte Birth of Nations',
+        'activacion_mensaje_titulo' => "Benvingut a Birth of Nations, ",
+        'activacion_mensaje_cuerpo' => "però abans és necessari que activis el teu compte per poder jugar.
+            Només necessites fer click a l'enllaç següent per activar el compte",
+        'activacion_mensaje_nick' => "Usuari registrat: ",
+        'activacion_mensaje_mensaje1' => "Si no t'has registrat a Birth of Nations, senzillament ignora el correu.",
+        'activacion_mensaje_firma' => "Cordialment, l'equip de BirthofNations",
+        );
+
+
+    $signup_form = array (
+        'signup_title' => "Formulari de registre"    ,
+        'signup_username' => "Nom d'usuari"    ,
+        'signup_username_info' => "Caràcters d' A-z, mínim 5 caràcters "    ,
+        'signup_pass' => 'Contrasenya',
+        'signup_pass_info' => "Mínim 5 caràcters, màxim 12 caràcters, lletres i símbols"    ,
+        'signup_pass2' => 'Repetir la contrasenya',
+        'signup_pass_info2' => "Ha de ser igual que l'anterior"    ,
+        'signup_referer' => 'Referent',
+        'signup_mail' => "E-mail"    ,
+        'signup_mail_info' => 'Escriu un correu vàlid, si us plau',// No s'utilitza?
+        'signup_submit' => "Formulari de registre"    ,// No s'utilitza?
+    );
+
+    $login_form = array (
+        'login_nonick' => "Falta l'usuari",
+        'login_nopass' => "Falta la contrasenya",
+        'login_nomatch' => "La combinació d'usuari/contrasenya no és la correcta [0x01]" ,
+        'login_nomatch2' => "La combinació d'usuari/contrasenya no és la correcta [0x02]",
+
+    );
+    ?>
