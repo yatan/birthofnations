ba667610ae07b4f4abb3a496fd8590972e1ffc1a
yatan87@gmail.com <yatan87@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2011-06-26 12:28:17 +0000

    formulario registro a partir de link con datos

--- trunk/registro.php
+++ trunk/registro.php
@@ -1,3 +1,22 @@
+<?
+
+//registro.php?referer=[array codificado con base64]=>[referer][email]
+if(!isset($_GET['referer']))
+    die("Error en el enlace");
+
+$datos = $_GET['referer'];
+$datos = base64_decode($datos);
+$array = explode(",", $datos);
+
+$referer = $array[0];
+$email = $array[1];
+
+
+?>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <link type="text/css" href="css/registro.css" rel="Stylesheet" />
 
 <div class="wrapper">	
@@ -19,12 +38,14 @@
 				<label for="password2">Repetir Contraseña <span style=color:green>Debe ser igual a la anterior</span></label>
 				<input tabindex="4" name="password2" id="password2" type="password" class="text" value="" />
 				
-				<label for="email">Email </label>
-					
-				<span style=color:green>Escribe un email válido por favor</span>
-						
-				</label>
-				<input tabindex="5" name="email" id="email" type="text" class="text" value="" />
+                                
+                                <label for="referer">Referido </label>					
+		
+                                
+				<input tabindex="5" name="referer" disabled id="email" type="text" class="text" value="<? echo $referer; ?>" />
+                                
+				<label for="email">Email</label>
+                                <input tabindex="5" name="email" disabled id="email" type="text" class="text" value="<? echo $email; ?>" />
 				<div>
 					<input tabindex="6" name="send" id="send" type="submit" class="submit" value="Enviar formulario" />
 				</div>
