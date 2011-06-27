0d6e91d7b2027799c6bfbab48c7d8208acf3228e
Etchner@gmail.com <Etchner@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2011-06-27 19:46:16 +0000

    gracias Morri ;)

---
+++ trunk/i18n/eu_ES.php
@@ -0,0 +1,35 @@
+<?php
+
+$mail_activation = array (
+    'activacion_titulo' => 'Activación de su cuenta Birth of Nations',
+    'activacion_mensaje_titulo' => "Bienvenido a Birth of Nations, ",
+    'activacion_mensaje_cuerpo' => "pero antes es necesario que actives la cuenta para poder jugar.
+        Solo necesitas hacer click en el siguiente link para activar la cuenta.",
+    'activacion_mensaje_nick' => "Usuario registrado: ",    
+    'activacion_mensaje_mensaje1' => "Si no te has registrado en Birth of Nations, simplemente ignora el correo.",
+    'activacion_mensaje_firma' => "Atte. El equipo de BirthofNations",    
+    );
+
+
+$signup_form = array (
+    'signup_title' => "Erregistro Formularioa"    ,
+    'signup_username' => " Erabiltzaile Izena"    ,
+    'signup_username_info' => "A eta Z bitarteko karaktereak, gutxieneko 5 karaktere "    ,
+    'signup_pass' => 'Pasahitza',
+    'signup_pass_info' => "gutxieneko 5 karaktere, Gehienezko 12 karaktere, letrak zein simboloak erabil ditzakezu"    ,
+    'signup_pass2' => 'Pasahitza eskatu',
+    'signup_pass_info2' => "Lehen erabili duzun pasahitz berdina izan behar da "    ,
+    'signup_referer' => 'Babeslea',
+    'signup_mail' => "email-a"    ,
+    'signup_mail_info' => 'Escribe un email válido por favor',// No se usa??
+    'signup_submit' => "Formulario de Registro"    ,// No se usa??
+);
+
+$login_form = array (
+    'login_nonick' => "Erabiltzailea falta da",
+    'login_nopass' => "Pasahitza falta da",
+    'login_nomatch' => "Sartu duzun erabiltzailea/pasahitza ez da egokia [0x01]" ,
+    'login_nomatch2' => "Sartu duzun erabiltzailea/pasahitza ez da egokia [0x02]",
+);
+?>
+    
