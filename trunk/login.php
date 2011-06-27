b824e6ae4ccd14e41e35692359cb89b1935f33f5
airumas@gmail.com <airumas@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2011-06-27 21:34:22 +0000

    Puesto en el login el anti baneados

--- trunk/login.php
+++ trunk/login.php
@@ -7,22 +7,29 @@ select_lang();
  * and open the template in the editor.
  */
 
-if(!isset($_POST['nick']) || $_POST['nick']=="")
+if(!isset($_POST['nick']) || $_POST['nick']=="") //Si no introduce el nick
 die($login_form['login_nonick']);
 else
 $user = $_POST['nick'];
 
-if(!isset($_POST['pass']) || $_POST['pass']=="")
+if(!isset($_POST['pass']) || $_POST['pass']=="") //Si no introduce la pass
 die($login_form['login_nopass']);
 else
 $pass = md5($_POST['pass']);
 
-$consulta = sql("SELECT nick FROM usuarios WHERE nick='$user'");
-if($consulta==false)
+$consulta = sql("SELECT nick, id FROM usuarios WHERE nick='$user'");
+
+if($consulta==false)//El usuario no esta en la BD
     die($login_form['login_nomatch']);
 else
+    $consulta = checkban($consulta['id']);
+    
+if($consulta == true ) //Si esta ban    
+    die($login_form['login_banned']);
+else
     $consulta = sql("SELECT id_usuario FROM usuarios WHERE nick='$user' AND password='$pass'");
-if($consulta==false)
+
+if($consulta==false)//No concuerdan user y pass
     die($login_form['login_nomatch2']);
 else
 {
