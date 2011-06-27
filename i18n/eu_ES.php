af252dc1c4d439b011a0d9065ca3560022ebb466
Etchner@gmail.com <Etchner@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2011-06-27 19:47:04 +0000

    fulmino

--- i18n/eu_ES.php
+++ i18n/eu_ES.php
@@ -1,14 +1,5 @@
 <?php
 
-$mail_activation = array (
-    'activacion_titulo' => 'Activación de su cuenta Birth of Nations',
-    'activacion_mensaje_titulo' => "Bienvenido a Birth of Nations, ",
-    'activacion_mensaje_cuerpo' => "pero antes es necesario que actives la cuenta para poder jugar.
-        Solo necesitas hacer click en el siguiente link para activar la cuenta.",
-    'activacion_mensaje_nick' => "Usuario registrado: ",    
-    'activacion_mensaje_mensaje1' => "Si no te has registrado en Birth of Nations, simplemente ignora el correo.",
-    'activacion_mensaje_firma' => "Atte. El equipo de BirthofNations",    
-    );
 
 
 $signup_form = array (
