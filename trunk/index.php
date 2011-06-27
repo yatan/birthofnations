1d9c3f2945f7c55876dfd72d356c50c4364a440b
EmmaFDAI@gmail.com <EmmaFDAI@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2011-06-27 20:26:05 +0000

    Primer Dia Kana Estilos :)

--- trunk/index.php
+++ trunk/index.php
@@ -1,48 +1,33 @@
-<?
-
-include_once("include/funciones.php");
-
+<?php
+	include_once("./include/funciones.php");
 ?>
-<!--
-To change this template, choose Tools | Templates
-and open the template in the editor.
--->
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
-  <head>
-    <title></title>
-	<link type="text/css" href="css/css.css" rel="Stylesheet" /> 
-	<link type="text/css" href="css/ui-lightness/jquery-ui-1.8.13.custom.css" rel="stylesheet" />	
-	<script type="text/javascript" src="js/jquery-1.6.1.min.js"></script>
-	<script type="text/javascript" src="js/jquery-ui-1.8.13.custom.min.js"></script>
+    <head>
+        <title>Birth Of Nations</title>
+        <link type="text/css" href="css/css.css" rel="Stylesheet" /> 
+        <link type="text/css" href="css/ui-lightness/jquery-ui-1.8.13.custom.css" rel="stylesheet" />
+        <link type="text/css" href="css/menu_style.css" rel="stylesheet" />
+        <script type="text/javascript" src="js/jquery-1.6.1.min.js"></script>
+        <script type="text/javascript" src="js/jquery-ui-1.8.13.custom.min.js"></script>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	
-
-  </head>
-  <body>
-  
-    <div id="top"> 
-    </br>
-    </div>
-    
-    <p>TODO write content</p>
-	
-	<br/>
-
-	
-        <br><br>
-        
-        <div id="login" style="margin-left:50%;">
-            <form action="login.php" method="POST">
-                <h2>Login</h2>
-                <label for="nick">Nick:<input tabindex="1" type="text" name="nick"></label><br>
-                <label for="pass">Password:<input tabindex="2" type="password" name="pass"></label><br>
-                <input type="submit">
-            </form>
-        </div>
-        
-	<div id="pie"> 
-		<center>Estado mysql: <? online(); ?> Copyright 2011 Batamanta Team</center> 
-	</div> 
-  </body>
+    </head>
+    <body> 
+        <div class="blur">
+            <div class="shadow">
+                <div id="contenido">
+                    <?php include("cabecera.php");?><br/>
+                    <div id="login">
+                        <form action="login.php" method="POST">
+                            <h2>Login</h2>
+                            <label for="nick">Nick:<input tabindex="1" type="text" name="nick"></label><br>
+                            <label for="pass">Password:<input tabindex="2" type="password" name="pass"></label><br>
+                            <input type="submit">
+                        </form>
+                    </div><!--login-->
+                    <?php include("pie.php");?>
+                </div><!-- contenido -->
+            </div><!-- shadow -->
+        </div><!-- blur -->
+    </body>
 </html>
---
+++ trunk/pie.php
@@ -0,0 +1,3 @@
+            <div id="pie"> 
+            	<center>Estado mysql: <? online(); ?> Copyright 2011 Batamanta Team</center> 
+            </div> <!--pie-->
