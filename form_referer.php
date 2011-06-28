288f57c6868e7ec5108be315379bbace284a5882
yatan87@gmail.com <yatan@users.noreply.github.com>
2011-06-28 21:42:55 +0000

    Formulario para enviar invitaciones

--- form_referer.php
+++ form_referer.php
@@ -1,16 +1,11 @@
-<?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
+<?
+    include_once("include/funciones.php");
+    $id = $_SESSION['id_usuario'];
+    echo "Tu id: ".$id;
 ?>
 
 <form action="form_referer1.php" method="post">
-    
-    Email: <input type="text" name="mail" /><br/>
+     Email: <input type="text" name="mail" /><br/>
     <input type="submit" value="Enviar invitacion" />
     
 </form>
