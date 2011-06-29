225ad51707f556e9f865385e5e0816bb45337764
Etchner@gmail.com <Etchner@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2011-06-29 20:21:07 +0000

    gracias Thindael

---
+++ i18n/pt_BR.php
@@ -0,0 +1,25 @@
+    <?php
+     
+    $signup_form = array (
+        'signup_title' => "Formulário de Registo"    ,
+        'signup_username' => "Nome do utilizador "    ,
+        'signup_username_info' => "Carácteres de A-Z, mínimo de 5 carácteres "    ,
+        'signup_pass' => 'Palavra-passe',
+        'signup_pass_info' => "Mínimo de 5 carácteres, máximo de 12 carácteres, letras e símbolos"    ,
+        'signup_pass2' => 'Confirmar a palavra passe',
+        'signup_pass_info2' => "Deve ser igual à anterior "    ,
+        'signup_referer' => 'Referidor',
+        'signup_mail' => "email"    ,
+        'signup_mail_info' => 'Escreve um email válido por favor',// Não se usa??
+        'signup_submit' => "Formulário de Registo"    ,// Não se usa??
+    );
+     
+    $login_form = array (
+        'login_nonick' => "Falta o nome de utilizador",
+        'login_nopass' => "Falta a palavra-passe",
+        'login_nomatch' => "A combinação nome de utilizador/palavra-passe não é correcta [0x01]" ,
+        'login_nomatch2' => "A combinação nome de utilizador/palavra-passe não é correcta [0x02]",
+        'login_banned' => "Foste alvo de hackers",
+       
+    );
+?>
