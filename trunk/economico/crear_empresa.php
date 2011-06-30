9f6db00a1229db56492aef49d70a9cab4d60aca5
airumas@gmail.com <airumas@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2011-06-30 23:26:29 +0000

    Creo la carpeta del modulo economico, hago un form de crear empresas y retoco la funcion que las crea para que añada el nombre y compruebe que no se repite.

---
+++ trunk/economico/crear_empresa.php
@@ -0,0 +1,26 @@
+<?php
+    
+    session_start();
+    include_once("funciones.php");
+    include_once("config_variables.php");
+    
+if (isset($_POST['tipo']) && $_POST['tipo'] != "" && strlen($_POST['tipo']) && isset($_POST['nombre']) && $_POST['nombre'] != "" && strlen($_POST['nombre'])){
+
+    $creador = $_SESSION['id_usuario'];
+    $nombre = $_POST['nombre'];
+    $sql = sql("SELECT id FROM empresas WHERE nombre_empresa = '" . $nombre ."'");//Comprobamos que no este el nombre cogido.
+    
+    if($sql != false )
+        die("El nombre esta cogido");
+    else
+        $gold = sql("SELECT gold FROM money WHERE id_usuario = " . $creador); //Vemos cuanto dinero tiene
+    
+    if ($gold < $precio_empresa[$tipo]) //Si tiene menos gold del que cuesta crearla
+        die("No hay sugus");
+    else {
+        sql("UPDATE money SET gold = gold - " . $precio_empresa[$tipo] . " WHERE id_usuario = " . $creador ); //Se quita el gold
+        sql("INSERT INTO empresas(id_propietario, tipo, nombre_empresa) VALUES $creador, $tipo, '$nombre' "); //se crea
+    }
+
+}
+?>
---
+++ trunk/economico/form_crear_empresa.php
@@ -0,0 +1,8 @@
+                    <div id="crear_empresa">
+                        <form action="crear_empresa.php" method="POST">
+                            <h2>Creacion de empresas:</h2>
+                            <label for="nombre">Nombre:<input tabindex="1" type="text" name="nick"></label><br>
+                            <label for="pass">Tipo:<select name="tipo"><option value=1>Fábrica de Sugus</option></label><br>
+                            <input type="submit">
+                        </form>
+                    </div><!--form de creacion de empresas-->
