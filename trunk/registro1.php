fb80788cc425f1b9e97e1503aa86f952cce874ec
yatan87@gmail.com <yatan87@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2011-07-01 18:26:17 +0000

    Bugfix Reparada la id del referer de la tabla usuarios

--- trunk/registro1.php
+++ trunk/registro1.php
@@ -42,7 +42,7 @@ $pass = md5($pass1);
 $id_referer = sql("SELECT id_usuario FROM usuarios WHERE nick='$referer'");
 $hoy = date("Y.n.j");
 
-sql("INSERT INTO usuarios(nick,password,email,fecha_registro,id_referer) VALUES ('$user','$pass','$email','$hoy','$referer')");//A�adir a la tabla de usuarios
+sql("INSERT INTO usuarios(nick,password,email,fecha_registro,id_referer) VALUES ('$user','$pass','$email','$hoy','$id_referer')");//A�adir a la tabla de usuarios
 $mi_id=sql("SELECT id_usuario FROM usuarios WHERE nick='$user'");
 sql("INSERT INTO money(id_usuario) VALUES ('$mi_id') "); //A�adir a la tabla de dinero
 
