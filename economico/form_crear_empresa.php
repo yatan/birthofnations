ea7411736565e54cfad27536616b18dfe781200f
yatan87@gmail.com <yatan@users.noreply.github.com>
2011-07-01 14:08:34 +0000

    Bugfix Pequeñas cosas Favicon

--- economico/form_crear_empresa.php
+++ economico/form_crear_empresa.php
@@ -2,7 +2,7 @@
                         <form action="crear_empresa.php" method="POST">
                             <h2>Creacion de empresas:</h2>
                             <label for="nombre">Nombre:<input tabindex="1" type="text" name="nick"></label><br>
-                            <label for="pass">Tipo:<select name="tipo"><option value=1>Fábrica de Sugus</option></label><br>
+                            <label for="pass">Tipo:<select name="tipo"><option value=1>FÃ¡brica de Sugus</option></select></label><br>
                             <input type="submit">
                         </form>
                     </div><!--form de creacion de empresas-->
