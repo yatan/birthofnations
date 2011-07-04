fd741c5d1f250ce0a5c7ba983e4898a6bd3cd0e7
yatan87@gmail.com <yatan@users.noreply.github.com>
2011-07-04 21:34:44 +0000

    bugfix Rutas y mas rutas

--- economico/form_crear_empresa.php
+++ economico/form_crear_empresa.php
@@ -1,7 +1,7 @@
                     <div id="crear_empresa">
-                        <form action="crear_empresa.php" method="POST">
+                        <form action="../economico/crear_empresa.php" method="POST">
                             <h2>Creacion de empresas:</h2>
-                            <label for="nombre">Nombre:<input tabindex="1" type="text" name="nick"></label><br>
+                            <label for="nombre">Nombre:<input tabindex="1" type="text" name="nombre"></label><br>
                             <label for="pass">Tipo:<select name="tipo"><option value=1>Fábrica de Sugus</option></select></label><br>
                             <input type="submit">
                         </form>
