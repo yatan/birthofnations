0007809096e1a8dbf74ebb6d0bfafbc6b464e73d
Etchner@gmail.com <Etchner@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2011-07-06 16:00:39 +0000

    primer intento de  fondo xD

--- login/login.php
+++ login/login.php
@@ -5,9 +5,9 @@
 <html>
     <head>
         <title>Birth Of Nations</title>
-        <link type="text/css" href="/css/css.css" rel="Stylesheet" /> 
-        <link type="text/css" href="/css/ui-lightness/jquery-ui-1.8.13.custom.css" rel="stylesheet" />
-        <link type="text/css" href="/css/menu_style.css" rel="stylesheet" />
+        <link type="text/css" href="./css/css.css" rel="Stylesheet" />
+        <link type="text/css" href="./css/ui-lightness/jquery-ui-1.8.13.custom.css" rel="stylesheet" />
+        <link type="text/css" href="./css/menu_style.css" rel="stylesheet" />
         <script type="text/javascript" src="js/jquery-1.6.1.min.js"></script>
         <script type="text/javascript" src="js/jquery-ui-1.8.13.custom.min.js"></script>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
@@ -28,7 +28,7 @@
                                 <tr>
                             <td><label for="pass">Password:</td><td><input tabindex="2" type="password" name="pass"></label></td>
                                <tr>     
-                            <td><input type="submit"></td>
+                                   <td><input type="submit" value="Login"></td>
                                </tr>
                             </table>
                         </form>
