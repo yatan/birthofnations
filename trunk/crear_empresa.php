581203fcd71f1a64f13a5ac4efde2b72fa9fa4c2
Etchner@gmail.com <Etchner@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2011-07-06 16:00:39 +0000

    primer intento de  fondo xD

---
+++ trunk/crear_empresa.php
@@ -0,0 +1,21 @@
+<?php
+    
+    session_start();
+    include_once("include/funciones.php");
+    include_once("config_variables.php");
+    
+if (isset($_POST['tipo']) && $_POST['tipo'] != "" && strlen($_POST['tipo'])){
+
+    $creador = $_SESSION['id_usuario'];
+    
+    
+    $gold = sql("SELECT gold FROM usuario WHERE id_usuario = " . $creador); //Vemos cuanto dinero tiene
+    
+    if ($gold >= $precio_empresa[$tipo]){ //Si tiene mas gold del que cuesta crearla
+        sql("UPDATE usuarios SET gold = gold - " . $precio_empresa[$tipo] . " WHERE id_usuario = " . $creador ); //Se quita el gold
+        sql("INSERT INTO empresas(id_propietario, tipo) VALUES $creador, $tipo"); //se crea
+    }
+    
+
+}
+?>
