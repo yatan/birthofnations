6def08d8de416fbf70c4cf63b4567aed0154decd
yatan87@gmail.com <yatan87@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2011-07-07 18:28:33 +0000

    empresas & bugfix Creacion de la pagina sobre la empresa Reparacion de rutas que tocó etch .htaccess actualizado

---
+++ trunk/economico/empresa.php
@@ -0,0 +1,46 @@
+<?
+
+include_once($_SERVER['DOCUMENT_ROOT']."include/funciones.php");
+
+if(!isset($_GET['id_empresa']))
+    die("Error: id no valido"); //Sustituir por error 404
+
+
+$id_empresa = $_GET['id_empresa'];
+
+
+class empresa
+{
+    public $id_empresa;
+    public $id_propietario;
+    public $nombre_empresa;
+    public $capital;
+    public $stock;
+    public $items_venta;
+    public $precio_venta;
+    public $pais;
+    public $region;
+    public $zona;
+    public $raw;
+    
+    function empresa($id){
+        $empresa = sql("SELECT * FROM empresas WHERE id='$id'");
+        
+        $this->id_empresa = $id;
+        $this->id_propietario = $empresa['id_propietario'];
+        $this->nombre_empresa = $empresa['nombre_empresa'];
+        $this->capital = $empresa['capital'];
+        $this->stock = $empresa['stock'];
+        $this->items_venta = $empresa['items_venta'];
+        $this->precio_venta = $empresa['precio_venta'];
+        $this->raw = $empresa['raw'];
+    }
+    
+}
+
+$empresa = new empresa($id_empresa);
+
+echo "<h1>Empresa</h1>";
+
+var_dump($empresa);
+?>
