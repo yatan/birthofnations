9d1d9e9ea9e06a2575a06abc0c54de73879037e6
yatan87@gmail.com <yatan@users.noreply.github.com>
2011-07-07 18:28:33 +0000

    empresas & bugfix Creacion de la pagina sobre la empresa Reparacion de rutas que tocó etch .htaccess actualizado

--- index.php
+++ index.php
@@ -19,11 +19,11 @@ else
 <html>
     <head>
         <title>Birth Of Nations</title>
-        <link type="text/css" href="./css/css.css" rel="Stylesheet" />
-        <link type="text/css" href="./css/ui-lightness/jquery-ui-1.8.13.custom.css" rel="stylesheet" />
-        <link type="text/css" href="./css/menu_style.css" rel="stylesheet" />
-        <script type="text/javascript" src="../js/jquery-1.6.1.min.js"></script>
-        <script type="text/javascript" src="../js/jquery-ui-1.8.13.custom.min.js"></script>
+        <link type="text/css" href="/css/css.css" rel="Stylesheet" />
+        <link type="text/css" href="/css/ui-lightness/jquery-ui-1.8.13.custom.css" rel="stylesheet" />
+        <link type="text/css" href="/css/menu_style.css" rel="stylesheet" />
+        <script type="text/javascript" src="/js/jquery-1.6.1.min.js"></script>
+        <script type="text/javascript" src="/js/jquery-ui-1.8.13.custom.min.js"></script>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     </head>
     <body> 
@@ -34,11 +34,12 @@ else
                     <div id="menu">
                         <h2>Menu</h2>
                         
-                        <p><a href="./">Inicio</a> - Economia - Militar - Politica - Perfil - <a href="./logout">Logout</a></p>
+                        <p><a href="./">Inicio</a> - Economia - Militar - Politica - Perfil - <a href="/logout">Logout</a></p>
                         <p><a href="./crear_empresa">Crear Empresa</a>
                         
                     </div><!--menu-->
                     <div id="cuerpo">
+                        <center>
                         <?
                         
                         if(isset($_GET['mod']))
@@ -48,11 +49,15 @@ else
                             case "crear_empresa":
                                 include("economico/form_crear_empresa.php");
                                 break;
+                            case "empresa":
+                                include("economico/empresa.php");
+                                break;
                             default :
                                 die($_GET['mod']); //Default por si se pone algo incorrecto. Al futuro hay que cambiarlo
                             }
                         
                         ?>
+                        </center>
                     </div><!--menu-->
                     <?php include("pie.php");?>
                 </div><!-- contenido -->
--- login/login.php
+++ login/login.php
@@ -5,11 +5,11 @@
 <html>
     <head>
         <title>Birth Of Nations</title>
-        <link type="text/css" href="./css/css.css" rel="Stylesheet" />
-        <link type="text/css" href="./css/ui-lightness/jquery-ui-1.8.13.custom.css" rel="stylesheet" />
-        <link type="text/css" href="./css/menu_style.css" rel="stylesheet" />
-        <script type="text/javascript" src="js/jquery-1.6.1.min.js"></script>
-        <script type="text/javascript" src="js/jquery-ui-1.8.13.custom.min.js"></script>
+        <link type="text/css" href="/css/css.css" rel="Stylesheet" />
+        <link type="text/css" href="/css/ui-lightness/jquery-ui-1.8.13.custom.css" rel="stylesheet" />
+        <link type="text/css" href="/css/menu_style.css" rel="stylesheet" />
+        <script type="text/javascript" src="/js/jquery-1.6.1.min.js"></script>
+        <script type="text/javascript" src="/js/jquery-ui-1.8.13.custom.min.js"></script>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     </head>
     <body> 
