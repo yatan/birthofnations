9c831e6b078082a54291e5b6e36774043361c88d
yatan87@gmail.com <yatan87@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2011-07-10 14:00:23 +0000

    primera cosa con jquery

--- trunk/economico/form_crear_empresa.php
+++ trunk/economico/form_crear_empresa.php
@@ -1,8 +1,19 @@
-                    <div id="crear_empresa">
-                        <form action="../economico/crear_empresa.php" method="POST">
-                            <h2>Creacion de empresas:</h2>
-                            <label for="nombre">Nombre:<input tabindex="1" type="text" name="nombre"></label><br>
-                            <label for="pass">Tipo:<select name="tipo"><option value=1>Fábrica de Sugus</option></select></label><br>
-                            <input type="submit">
-                        </form>
-                    </div><!--form de creacion de empresas-->
+
+<div id="crear_empresa">
+    <form id="empresa" action="../economico/crear_empresa.php" method="POST">
+        <h2>Creacion de empresas:</h2>
+        <label for="nombre">Nombre:<input tabindex="1" type="text" name="nombre"></label><br>
+        <label for="pass">Tipo:<select name="tipo"><option value=1>Fábrica de Sugus</option></select></label><br>
+        <input type="button" id="enviar" value="Enviar">
+    </form>
+</div><!--form de creacion de empresas-->
+<script>
+    $('#enviar').click(function() {
+  	$.post("../economico/crear_empresa.php", $("#empresa").serialize(),
+        function(data){
+                        alert(data);
+                      } );
+   
+    });
+
+</script>
