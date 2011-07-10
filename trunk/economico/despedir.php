56166799e099a22d3a2242ce329a1bb62f1278e0
airumas@gmail.com <airumas@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2011-07-10 20:14:35 +0000

    Despedir gente es chachi

--- trunk/economico/despedir.php
+++ trunk/economico/despedir.php
@@ -1,11 +1,15 @@
 <?php
-if(!isset($_POST['id_worker']))
-    die("Who?");
-    
+
 //Mecanismos de seguridad, comprobar que estas logueado como admin y tal
 
+include_once($_SERVER['DOCUMENT_ROOT']."/include/funciones.php");
+
+$id_usuario = $_SESSION['id_usuario'];
+
+if (isset($_GET['id_worker']) && $_GET['id_worker'] != "" && strlen($_GET['id_worker'])>0 ){
 
 //Mecanismo del despido
-sql("UPDATE usuarios SET id_empresa = 0 WHERE id_usuario = " . $_POST['id_worker']);
+sql("UPDATE usuarios SET id_empresa = 0 WHERE id_usuario = " . $_GET['id_worker']);
 
+}
 ?>
