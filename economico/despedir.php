d0c83a0332de7bee31fca8db9411df208811500e
airumas@gmail.com <airumas@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2011-07-10 22:20:00 +0000

    Guarda el salario en la tabla del usuario al aceptar la oferta y al despedir lo pone a 0.

--- economico/despedir.php
+++ economico/despedir.php
@@ -9,7 +9,7 @@ $id_usuario = $_SESSION['id_usuario'];
 if (isset($_GET['id_worker']) && $_GET['id_worker'] != "" && strlen($_GET['id_worker'])>0 ){
 
 //Mecanismo del despido
-sql("UPDATE usuarios SET id_empresa = 0 WHERE id_usuario = " . $_GET['id_worker']);
+sql("UPDATE usuarios SET id_empresa = 0, salario = 0 WHERE id_usuario = " . $_GET['id_worker']);
 
 }
 ?>
--- economico/poner_oferta.php
+++ economico/poner_oferta.php
@@ -6,8 +6,9 @@
 	
 if (isset($_POST['salario']) && $_POST['salario'] != "" && is_numeric($_POST['salario']) && isset($_POST['cantidad']) && $_POST['cantidad'] != "" && is_numeric($_POST['cantidad']))
 {
-    
+        
     sql("INSERT INTO mercado_trabajo(id_pais, id_empresa, salario, cantidad) VALUES ('".$_POST['id_pais']."','".$_POST['id_empresa']."','".$_POST['salario']."','".$_POST['cantidad']."') ");
+
     echo "Oferta añadida correctamente"; 
 } else {
     
