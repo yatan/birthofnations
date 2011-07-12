1613104b184f6b15fba4dd8eb0dd38d373b83c33
yatan87@gmail.com <yatan87@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2011-07-12 15:09:08 +0000

    arreglado links empresas

--- trunk/economico/empresas.php
+++ trunk/economico/empresas.php
@@ -15,7 +15,7 @@ echo"<h1>Mis empresas</h1>";
 $sql = sql("SELECT * FROM empresas WHERE id_propietario='".$_SESSION['id_usuario']."'");
 
 foreach ($sql as $empresa) {
-    echo "<a href='/".$_GET['lang']."/".$empresa['id_empresa']."'>".$empresa['nombre_empresa']."</a><br/>";
+    echo "<a href='/".$_GET['lang']."/empresa/".$empresa['id_empresa']."'>".$empresa['nombre_empresa']."</a><br/>";
 }
 ?>
 
