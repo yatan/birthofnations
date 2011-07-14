a53e50379219ef7c47e850327b91a8401f3ba1c9
airumas@gmail.com <airumas@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2011-07-14 22:00:22 +0000

    Azúcar!!

--- economico/form_crear_empresa.php
+++ economico/form_crear_empresa.php
@@ -3,7 +3,10 @@
     <form id="empresa" action="../economico/crear_empresa.php" method="POST">
         <h2>Creacion de empresas:</h2>
         <label for="nombre">Nombre:<input tabindex="1" type="text" name="nombre"></label><br>
-        <label for="pass">Tipo:<select name="tipo"><option value=1>Fábrica de Sugus</option></select></label><br>
+        <label for="pass">Tipo:<select name="tipo">
+                <option value=1>Fábrica de Sugus</option>
+                <option value=2>Fábrica de Az�car</option></select>
+        </label><br>
         <input type="button" id="enviar" value="Enviar">
     </form>
 </div><!--form de creacion de empresas-->
