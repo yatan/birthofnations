ab017fd221029f8f9850c1734606ab0f2d1dad8e
airumas@gmail.com <airumas@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2011-07-16 14:34:12 +0000

    Poner jefe en mercado laboral

--- trunk/economico/mercado_trabajo.php
+++ trunk/economico/mercado_trabajo.php
@@ -6,11 +6,12 @@ $id_pais = 1; //Sea como sea que se seleccione
 
 $ofertas = mysql_query("SELECT * FROM mercado_trabajo WHERE id_pais = " . $id_pais . " ORDER BY salario DESC");
 
-echo "<table><tr><th>Empresa</th><th>Salario</th><th>Puestos</th></tr>";
+echo "<table><tr><th>Empresa</th><th>Jefe</th><th>Salario</th><th>Puestos</th></tr>";
 
 while ($oferta = mysql_fetch_array($ofertas)){
     $sql = sql("SELECT nombre_empresa FROM empresas WHERE id_empresa='".$oferta['id_empresa']."'");
-    echo "<tr><td>".$sql."</td><td>". $oferta['salario'] ."</td><td>". $oferta['cantidad'] .'</td><td>[<a href="/economico/aceptar_trabajo.php?oferta='.$oferta['id_oferta'].'">Aceptar</a>]</td></tr>';
+    $jefe = sql("SELECT nick FROM usuarios WHERE id_usuario = '". $oferta['id_jefe'] ."' " );
+    echo "<tr><td>".$sql."</td><td>". $jefe ."</td><td>". $oferta['salario'] ."</td><td>". $oferta['cantidad'] .'</td><td>[<a href="/economico/aceptar_trabajo.php?oferta='.$oferta['id_oferta'].'">Aceptar</a>]</td></tr>';
         
 }
 echo "</table>";
