a4a854bf88ad04b62c0157250aa79df5fcb3fe0a
yatan87@gmail.com <yatan@users.noreply.github.com>
2011-07-16 15:00:39 +0000

    algo de colorido en la pagina principal

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ else
                         <?
                         
                         if(isset($_GET['mod']))
-                            
+                        {    
                             switch($_GET['mod'])
                             {
                             case "crear_empresa":
@@ -52,7 +52,54 @@ else
                             default :
                                 die($_GET['mod']); //Default por si se pone algo incorrecto. Al futuro hay que cambiarlo
                             }
-                        
+                        }
+                        else
+                        {
+                            //Aqui va lo del centro de la pagina principal
+                            ?>
+                            <div id="columna1">
+                                <h1>Noticias</h1>
+                                <li>Ya se puede logear !</li>
+                            </div>
+
+                            <div id="columna2">
+                                <script src="http://widgets.twimg.com/j/2/widget.js"></script>
+                                <script>
+                                new TWTR.Widget({
+                                  version: 2,
+                                  type: 'search',
+                                  search: 'beiber',
+                                  interval: 6000,
+                                  title: 'twitter de prueba',
+                                  subject: 'En busca del beiber perdido',
+                                  width: 250,
+                                  height: 300,
+                                  theme: {
+                                    shell: {
+                                      background: '#8ec1da',
+                                      color: '#ffffff'
+                                    },
+                                    tweets: {
+                                      background: '#ffffff',
+                                      color: '#444444',
+                                      links: '#1985b5'
+                                    }
+                                  },
+                                  features: {
+                                    scrollbar: false,
+                                    loop: true,
+                                    live: true,
+                                    hashtags: true,
+                                    timestamp: true,
+                                    avatars: true,
+                                    toptweets: true,
+                                    behavior: 'default'
+                                  }
+                                }).render().start();
+                                </script>
+                            </div>
+                            <?
+                        }
                         ?>
                         </center>
                     </div><!--menu-->
