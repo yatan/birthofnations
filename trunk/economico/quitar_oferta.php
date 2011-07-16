4a9e57732d4af3815b19a8178b391b100cc6c00c
airumas@gmail.com <airumas@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2011-07-16 15:47:36 +0000

    Quitar ofertas del mercado y mostrarlas

---
+++ trunk/economico/quitar_oferta.php
@@ -0,0 +1,18 @@
+<?php
+
+//Mecanismos de seguridad, comprobar que estas logueado como admin y tal
+
+include_once($_SERVER['DOCUMENT_ROOT']."/include/funciones.php");
+
+$id_usuario = $_SESSION['id_usuario'];
+
+if (isset($_GET['id_oferta']) && $_GET['id_oferta'] != "" && strlen($_GET['id_oferta'])>0 ){
+
+//Mecanismo de quitar oferta
+sql("DELETE FROM mercado_trabajo WHERE id_oferta = '". $_GET['id_oferta'] ."'");
+
+}
+?>
+<script>
+    window.history.go(-1);
+</script>
