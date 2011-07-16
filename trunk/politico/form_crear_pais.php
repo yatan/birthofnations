5cd54079c60532600c2ff4aa73e92c71bffc7dc9
airumas@gmail.com <airumas@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2011-07-16 16:38:03 +0000

    Al crear un pai spide un nombre para la moneda y lo añade a las tablas

---
+++ trunk/politico/form_crear_pais.php
@@ -0,0 +1,8 @@
+                    <div id="crear_partido">
+                        <form action="../politico/crear_pais.php" method="POST">
+                            <h2>Creacion de paises:</h2>
+                            <label for="nombre">Nombre:<input tabindex="1" type="text" name="name"></label><br>
+                            <label for="moneda">Moneda:<input tabindex="1" type="text" name="moneda"></label><br>
+                            <input type="submit">
+                        </form>
+                    </div><!--form de creacion de pais-->
