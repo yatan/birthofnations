8660671d69bd78ca764fb2d6a32737851407b387
yatan87@gmail.com <yatan@users.noreply.github.com>
2011-07-16 19:04:37 +0000

    modificaciones leves

--- index.php
+++ index.php
@@ -69,39 +69,36 @@ else
 
                             <div id="columna2">
                                 <script src="http://widgets.twimg.com/j/2/widget.js"></script>
-                                <script>
-                                new TWTR.Widget({
-                                  version: 2,
-                                  type: 'search',
-                                  search: 'beiber',
-                                  interval: 6000,
-                                  title: 'twitter de prueba',
-                                  subject: 'En busca del beiber perdido',
-                                  width: 250,
-                                  height: 300,
-                                  theme: {
-                                    shell: {
-                                      background: '#8ec1da',
-                                      color: '#ffffff'
-                                    },
-                                    tweets: {
-                                      background: '#ffffff',
-                                      color: '#444444',
-                                      links: '#1985b5'
-                                    }
-                                  },
-                                  features: {
-                                    scrollbar: false,
-                                    loop: true,
-                                    live: true,
-                                    hashtags: true,
-                                    timestamp: true,
-                                    avatars: true,
-                                    toptweets: true,
-                                    behavior: 'default'
-                                  }
-                                }).render().start();
-                                </script>
+<script>
+new TWTR.Widget({
+  version: 2,
+  type: 'profile',
+  rpp: 4,
+  interval: 6000,
+  width: 250,
+  height: 300,
+  theme: {
+    shell: {
+      background: '#333333',
+      color: '#ffffff'
+    },
+    tweets: {
+      background: '#000000',
+      color: '#ffffff',
+      links: '#4aed05'
+    }
+  },
+  features: {
+    scrollbar: false,
+    loop: false,
+    live: true,
+    hashtags: true,
+    timestamp: true,
+    avatars: false,
+    behavior: 'all'
+  }
+}).render().setUser('birthofn').start();
+</script>
                             </div>
                             <?
                         }
