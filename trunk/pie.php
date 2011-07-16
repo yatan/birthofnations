099fa130cb6686cdf7659ec2212286d2f4e2cfbc
yatan87@gmail.com <yatan87@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2011-07-16 19:04:37 +0000

    modificaciones leves

--- trunk/pie.php
+++ trunk/pie.php
@@ -1,3 +1,3 @@
             <div id="pie"> 
-            	<center>Estado mysql: <? online(); ?> <a href="COPYRIGHT">Copyright 2011 Batamanta Team</a> r119</center> 
+            	<center>Estado mysql: <? online(); ?> <a href="COPYRIGHT">Copyright 2011 Batamanta Team</a> r140</center> 
             </div> <!--pie-->
