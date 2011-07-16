3a72cd2a94db7293fe4de406dc5cc5d38020b944
airumas@gmail.com <airumas@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2011-07-16 16:38:03 +0000

    Al crear un pai spide un nombre para la moneda y lo añade a las tablas

--- politico/crear_pais.php
+++ politico/crear_pais.php
@@ -1,14 +1,20 @@
 <?php
 
-    session_start();
     include_once("../include/funciones.php");
     include_once("../include/config_variables.php");
 
-if (isset($_POST['name']) && $_POST['name'] != "" && strlen($_POST['name']) != 0){
-
+    var_dump(isset($_POST['name']));
+    
+if (isset($_POST['name']) && $_POST['name'] != "" && strlen($_POST['name']) != 0 && isset($_POST['moneda']) && strlen($_POST['moneda']) == 3 ) {
+echo "A";
     $creador = $_SESSION['id_usuario'];
-    sql("INSERT INTO country(name, date_of_birth) VALUES $name, GETDATE()"); 
-
-
-}
+    $name = $_POST['name'];
+    
+    sql("INSERT INTO country(name, fundador, date_of_birth) VALUES ('$name', '$creador', Now() )"); 
+    sql("ALTER TABLE money ADD COLUMN ". $_POST['moneda'] ." int(11) NOT NULL DEFAULT 0");
+    sql("ALTER TABLE empresas ADD COLUMN ". $_POST['moneda'] ." int(11) NOT NULL DEFAULT 0");
+    
+    //Aqui falta añadirla a la lista de monedas de moneda_local.php
+    
+} else { echo"B"; }
 ?>
