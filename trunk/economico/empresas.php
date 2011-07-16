70a19bed831446a29dda821626716610b5950668
yatan87@gmail.com <yatan87@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2011-07-16 15:44:34 +0000

    Bug arreglado bug cuando solo se tiene una empresa

--- trunk/economico/empresas.php
+++ trunk/economico/empresas.php
@@ -11,11 +11,18 @@ include_once($_SERVER['DOCUMENT_ROOT']."/include/funciones.php");
 
 echo"<h1>Mis empresas</h1>";
 
+$cantidad_empresas = sql("SELECT COUNT(id_empresa) FROM empresas WHERE id_propietario='".$_SESSION['id_usuario']."'");
 
 $sql = sql("SELECT * FROM empresas WHERE id_propietario='".$_SESSION['id_usuario']."'");
 
+if($cantidad_empresas == 1)
+echo "<a href='/".$_GET['lang']."/empresa/".$sql['id_empresa']."'>".$sql['nombre_empresa']."</a><br/>";
+else {
+    
 foreach ($sql as $empresa) {
     echo "<a href='/".$_GET['lang']."/empresa/".$empresa['id_empresa']."'>".$empresa['nombre_empresa']."</a><br/>";
+    }
+
 }
 ?>
 <br/>
