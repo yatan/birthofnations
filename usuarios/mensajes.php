5645ef8f0edd013a018eb1172a45d5f7608429ff
airumas@gmail.com <airumas@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2011-07-17 22:06:12 +0000

    Perdon

--- usuarios/mensajes.php
+++ usuarios/mensajes.php
@@ -9,20 +9,20 @@ include_once($_SERVER['DOCUMENT_ROOT']."/include/funciones.php");
 
 $cantidad = sql("SELECT COUNT(id) FROM messages WHERE id_receptor ='".$_SESSION['id_usuario']."'");
 
-$sql = sql("SELECT * FROM messages WHERE id_receptor = " . $_SESSION['id_usuario']);
+$sql = sql2("SELECT * FROM messages WHERE id_receptor = " . $_SESSION['id_usuario']);
+
 
 echo "<table>";
 
-    if($cantidad > 1)
+    if($cantidad != 0)
     {
     foreach($sql as $msg){
-        echo "<tr><td>" . $msg['nick_emisor'] . ": </td><td>". $msg['mensaje'] ."</td></tr>";
+        echo '<tr><td>[<a href="/usuarios/borrar_mensaje?id='. $msg['id'] .'">Borrar</a>]</td><td>' . $msg['nick_emisor'] . ": </td><td>". $msg['mensaje'] ."</td></tr>";
         }
     }
-    elseif($cantidad==0)
+    else{
         echo "<p>".$txt['no_mensajes']."</p>";
-    elseif($cantidad==1)
-        echo '<tr><td>[<a href="/usuarios/borrar_mensaje.php?id=' .$sql['id']. '">Borrar</a>]</td><td>'. $sql['nick_emisor'] .": </td> <td>". $sql['mensaje'] .'</td></tr>';
+    }
       
 echo "</table>";
 
