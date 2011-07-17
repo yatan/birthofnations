d969f76ee179c4c1417431cd41742d7679d7be88
yatan87@gmail.com <yatan87@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2011-07-17 19:44:27 +0000

    retoques en el index

---
+++ trunk/columna2.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+
+
+<script src="http://widgets.twimg.com/j/2/widget.js"></script>
+<script>
+new TWTR.Widget({
+  version: 2,
+  type: 'search',
+  search: 'from:birthofn OR @birthofn OR #birthofn',
+  interval: 6000,
+  title: 'Twitter de #birthofn',
+  subject: '@birthofn',
+  width: 250,
+  height: 300,
+  theme: {
+    shell: {
+      background: '#8ec1da',
+      color: '#ffffff'
+    },
+    tweets: {
+      background: '#ffffff',
+      color: '#444444',
+      links: '#1985b5'
+    }
+  },
+  features: {
+    scrollbar: true,
+    loop: false,
+    live: true,
+    hashtags: true,
+    timestamp: true,
+    avatars: true,
+    toptweets: false,
+    behavior: 'all'
+  }
+}).render().start();
+</script>
--- trunk/index.php
+++ trunk/index.php
@@ -66,42 +66,11 @@ else
                             //Aqui va lo del centro de la pagina principal
                             ?>
                             <div id="columna1">
-                                <h1>Noticias</h1>
-                                <li>Ya se puede logear !</li>
+                               <? include("columna1.php"); ?>
                             </div>
 
                             <div id="columna2">
-                                <script src="http://widgets.twimg.com/j/2/widget.js"></script>
-<script>
-new TWTR.Widget({
-  version: 2,
-  type: 'profile',
-  rpp: 4,
-  interval: 6000,
-  width: 250,
-  height: 300,
-  theme: {
-    shell: {
-      background: '#333333',
-      color: '#ffffff'
-    },
-    tweets: {
-      background: '#000000',
-      color: '#ffffff',
-      links: '#4aed05'
-    }
-  },
-  features: {
-    scrollbar: false,
-    loop: false,
-    live: true,
-    hashtags: true,
-    timestamp: true,
-    avatars: false,
-    behavior: 'all'
-  }
-}).render().setUser('birthofn').start();
-</script>
+                               <? include("columna2.php"); ?>
                             </div>
                             <?
                         }
