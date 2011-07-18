0a3d1f15cb8f694e04fb1e73ab35fa8314382a36
yatan87@gmail.com <yatan@users.noreply.github.com>
2011-07-18 21:22:16 +0000

    error arreglado

--- usuarios/enviar_mensaje.php
+++ usuarios/enviar_mensaje.php
@@ -1,15 +1,15 @@
 
 <div id="crear_empresa">
-    <form id="empresa" action="../usuarios/envio_mensaje.php" method="POST">
-        <h2>Creacion de empresas:</h2>
-        <label for="nombre">Nombre:<input tabindex="1" type="text" name="nombre"></label><br>
+    <form id="empresa">
+        <h2>Envio de mensajes</h2>
+        <label for="nombre">Nick:<input tabindex="1" type="text" name="nombre"></label><br>
         <label for="nombre">Mensaje:<input tabindex="1" type="text" name="msj"></label><br>
         <input type="button" id="enviar" value="Enviar">
     </form>
 </div><!--form de creacion de empresas-->
 <script>
     $('#enviar').click(function() {
-  	$.post("../economico/crear_empresa.php", $("#empresa").serialize(),
+  	$.post("/usuarios/envio_mensaje.php", $("#empresa").serialize(),
         function(data){
                         alert(data);
                       } );
