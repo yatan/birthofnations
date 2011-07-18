380312c5b605ca66aa08ce87c9f289697143d61b
yatan87@gmail.com <yatan87@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2011-07-18 21:05:14 +0000

    algo de texto cutre para leer

--- trunk/login/login.php
+++ trunk/login/login.php
@@ -7,6 +7,9 @@
     <body> 
                 <div id="contenido">
                     <?php include("cabecera.php");?><br/>
+                    <div id="texto" style="color:black; font-size: large;">
+                        Birth of Nations es un juego online libre de estrategia política, económica y social. El desarrollo del juego se realiza de forma comunitaria, y sus propios jugadores deciden cual es el rumbo que toma el proyecto.
+                    </div>
                     <div id="login">
                         <form action="../login/logear.php" method="POST">
                             <h2>Login</h2>
