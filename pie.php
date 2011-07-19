d48d4503016b76cbcbcf11a612be381ed0476600
yatan87@gmail.com <yatan@users.noreply.github.com>
2011-07-19 17:31:04 +0000

    Comprobar si los servers funcionan

--- pie.php
+++ pie.php
@@ -1,3 +1,3 @@
             <div id="pie"> 
-            	<br/><center>Estado mysql: <? online(); ?> <a href="COPYRIGHT">Copyright 2011 Batamanta Team</a> r158</center> 
+            	<br/><center>Estado mysql: <? mysql_online(); ?> <a href="COPYRIGHT">Copyright 2011 Batamanta Team</a> r158</center> 
             </div> <!--pie-->
