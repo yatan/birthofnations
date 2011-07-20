121fd6d68018fccce409cbd2e812a1e59f4cad7c
airumas@gmail.com <airumas@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2011-07-20 20:22:12 +0000

    Mercado objetos

---
+++ economico/comprar.php
@@ -0,0 +1,40 @@
+<?php
+    
+    include_once($_SERVER['DOCUMENT_ROOT']."/include/funciones.php");
+    include_once($_SERVER['DOCUMENT_ROOT']."/include/config_variables.php");
+    include_once($_SERVER['DOCUMENT_ROOT']."/economico/moneda_local.php");
+    
+    if (isset($_POST['cantidad']) && $_POST['cantidad'] != "" && strlen($_POST['cantidad'])>0 ){
+        
+        //Sacamos los datos de la oferta 
+        $oferta = sql("SELECT * FROM mercado_objetos WHERE id_oferta = ". $_POST['id_oferta']);
+       
+        //Sacamos la cantidad de dinero que tiene el usuario
+        $dinero = sql("SELECT ". $moneda_local[$oferta['id_pais']] ." FROM money WHERE id_usuario = ". $_SESSION['id_usuario']);
+        
+        //Comprobamos que tiene suficiente Y quiero menos de los que hay        
+        if ( $dinero >= $oferta['precio']*$_POST['cantidad'] && $_POST['cantidad'] <= $oferta['cantidad'] ){
+            
+            //Los quitamos de la oferta
+            if($oferta['cantidad'] == $_POST['cantidad']){//Si no hay mas borramos
+                
+                sql("DELETE FROM mercado_objetos WHERE id_oferta = ". $_POST['id_oferta']);
+                
+            }else{//Si quedan quitamos los comprados
+                
+                sql("UPDATE mercado_objetos SET cantidad = cantidad - ". $_POST['cantidad'] ." WHERE id_oferta = ". $_POST['id_oferta']);
+                
+            }
+            
+            //Quitar dinero al user
+            sql("UPDATE money SET ". $moneda_local[$oferta['id_pais']] . " = ". $moneda_local[$oferta['id_pais']] ." - ". $oferta['precio']*$_POST['cantidad'] ." WHERE id_usuario = ". $_SESSION['id_usuario']);
+            //Meter en la empresa
+            sql("UPDATE empresas SET ". $moneda_local[$oferta['id_pais']] . " = ". $moneda_local[$oferta['id_pais']] ." + ". $oferta['precio']*$_POST['cantidad'] ." WHERE id_empresa = ". $oferta['id_empresa']);
+            
+        }else{
+            die("No tienes suficiente dinero");
+        }
+        
+        
+    }
+?>
