fa26f983dc92700ebaa14d39f98fbebfb16283b1
yatan87@gmail.com <yatan87@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2011-07-24 21:03:37 +0000

    gente online

--- trunk/pie.php
+++ trunk/pie.php
@@ -1,3 +1,13 @@
-            <div id="pie"> 
-            	<br/><center>Estado mysql: <? mysql_online(); ?> <a href="COPYRIGHT">Copyright 2011 Batamanta Team</a> r158</center> 
-            </div> <!--pie-->
+<?
+if(isset($_SESSION['id_usuario']))
+{
+    sql("UPDATE usuarios SET ultima_accion='".time()."' WHERE id_usuario='".$_SESSION['id_usuario']."'");
+}
+//Se calcula como maximo 15 minutos para diferenciar la gente online de la offline
+$calculo_minutos = time()-(60*15);
+$online = sql("SELECT COUNT(*) FROM usuarios WHERE ultima_accion >= '$calculo_minutos'");
+?>
+
+<div id="pie"> 
+    <br/><center>Hay: <? echo $online; ?> usuarios online Estado mysql: <? mysql_online(); ?> <a href="COPYRIGHT">Copyright 2011 Batamanta Team</a> r158</center> 
+</div> <!--pie-->
