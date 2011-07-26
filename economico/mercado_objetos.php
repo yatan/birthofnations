69d38b30f7f80e6ae8f25ff47abbad18a1709e7f
yatan87@gmail.com <yatan@users.noreply.github.com>
2011-07-26 21:17:43 +0000

    menu NO funcional para mercado de objetos

--- economico/mercado_objetos.php
+++ economico/mercado_objetos.php
@@ -1,5 +1,4 @@
 <?
-
 include_once($_SERVER['DOCUMENT_ROOT']."/include/funciones.php");
 
 if(isset($_GET['pais']))
@@ -8,9 +7,33 @@ else
     $pais = sql("SELECT id_pais FROM usuarios WHERE id_usuario='".$_SESSION['id_usuario']."'");
 
 //$objeto = $_GET['producto'];
+if(isset($_GET['producto']))
+    $objeto = $_GET['producto']; 
+else
+    $objeto="sugus";
+?>
 
+<link rel="stylesheet" type="text/css" href="/css/dd.css" />
+<script type="text/javascript" src="/js/jquery.dd.js"></script>
+
+<script language="javascript">
+$(document).ready(function(e) {
+try {
+$("select").msDropDown();
+} catch(e) {
+alert(e.message);
+}
+});
+</script>
+
+  <select style="width:200px;" name="webmenu" id="webmenu" onchange="showValue(this.value)">
+    <option value="calendar" selected="selected" title="/images/flag/es.png">Spain</option>
+    <option value="shopping_cart"  selected="selected" title="/images/flag/fr.png">France</option>
+  </select>
+
+<?
 echo "<h1>Mercado de objetos de: ".sql("SELECT name FROM country WHERE idcountry='$pais'")."</h1>";
-$objeto="sugus";
+
 
 $ofertas = sql2("SELECT * FROM mercado_objetos WHERE id_pais = " . $pais . " AND objeto = '". $objeto ."' ORDER BY precio ASC");
 
