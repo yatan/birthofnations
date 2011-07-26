eef5af9e03e297467927847fe742ac24ab535929
yatan87@gmail.com <yatan87@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2011-07-26 19:21:42 +0000

    mercado reparado

--- trunk/economico/mercado_objetos.php
+++ trunk/economico/mercado_objetos.php
@@ -2,10 +2,14 @@
 
 include_once($_SERVER['DOCUMENT_ROOT']."/include/funciones.php");
 
-//$pais = $_GET['id_pais']; 
+if(isset($_GET['pais']))
+    $pais = $_GET['pais']; 
+else
+    $pais = sql("SELECT id_pais FROM usuarios WHERE id_usuario='".$_SESSION['id_usuario']."'");
+
 //$objeto = $_GET['producto'];
 
-$pais = 1;
+echo "<h1>Mercado de objetos de: ".sql("SELECT name FROM country WHERE idcountry='$pais'")."</h1>";
 $objeto="sugus";
 
 $ofertas = sql2("SELECT * FROM mercado_objetos WHERE id_pais = " . $pais . " AND objeto = '". $objeto ."' ORDER BY precio ASC");
@@ -14,7 +18,7 @@ echo "<table><tr><th>Empresa</th><th>Precio</th><th>Cantidad</th><th>Comprar</th
 
 foreach ($ofertas as $oferta){
     
-    $empresa = sql("SELECT nombre_empresa FROM empresas WHERE id_empresa = " . $oferta['id_empresa']);
+    $empresa = sql("SELECT nombre_empresa FROM empresas WHERE id_empresa = '" . $oferta['id_empresa']."'");
     
     echo " <tr><td>". $empresa ."</td><td>". $oferta['precio'] ."</td><td>". $oferta['cantidad'] .'</td><td>
 <form action="../economico/comprar.php"  method="POST">
