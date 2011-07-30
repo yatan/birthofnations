93e99c2e7d7cb73d814d6c83f3571c2459b62dc8
yatan87@gmail.com <yatan87@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2011-07-30 12:01:22 +0000

    mejorado envio mensajes Creacion modulo periodistico

--- trunk/usuarios/enviar_mensaje.php
+++ trunk/usuarios/enviar_mensaje.php
@@ -3,7 +3,7 @@
     <form id="empresa">
         <h2>Envio de mensajes</h2>
         <label for="nombre">Nick:<input tabindex="1" type="text" name="nombre"></label><br>
-        <label for="nombre">Mensaje:<textarea tabindex="1" name="msj"></textarea></label><br>
+        <label for="markItUp">Mensaje:<textarea id="markItUp" cols="60" rows="10" tabindex="1" name="msj"></textarea></label><br>
         <input type="button" id="enviar" value="Enviar">
     </form>
 </div><!--form de creacion de empresas-->
