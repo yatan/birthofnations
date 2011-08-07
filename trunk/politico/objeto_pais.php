00c9ac2fcf5d42d6ebe1c577f53c3628221fd9b4
airumas@gmail.com <airumas@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2011-08-07 19:55:47 +0000

    Apañada la lista de lideres

--- trunk/politico/objeto_pais.php
+++ trunk/politico/objeto_pais.php
@@ -13,7 +13,7 @@ class pais {
         $this->nombre = sql("SELECT name FROM country WHERE idcountry = " . $id);
     }
     function list_leaders($id){
-        $sql = sql2("SELECT * FROM country_leaders WHERE idcountry = ". $id ." AND date_until  >= ". date("Y-m-d") );
+        $sql = sql2("SELECT * FROM country_leaders WHERE idcountry = ". $id ." AND ( date_until  >= ". date("Y-m-d") ." OR is_forever = 1 ) ORDER BY position ASC");
         return $sql;
     }
 
