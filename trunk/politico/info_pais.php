7ad88bc5bd224ba36864e75c150c078158056bec
airumas@gmail.com <airumas@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2011-08-07 23:31:25 +0000

    Pagina del pais. Regiones y cantidad de ciudadanos.

--- trunk/politico/info_pais.php
+++ trunk/politico/info_pais.php
@@ -19,6 +19,10 @@ $pais = new pais($id_pais);
 $nombre_pais = $pais->nombre;
 echo "<h1>$nombre_pais</h1>";
 
+echo "Poblacion actual: " . $pais->population($id_pais);
+
+echo "<h3>Líderes</h3>";
+
 $leaders = $pais->list_leaders($id_pais);
 
 echo "<table><tr><th>Nick</th><th>Posicion</th></tr>";
@@ -27,7 +31,7 @@ foreach ($leaders as $leader) {
 
     $name = sql("SELECT nick FROM usuarios WHERE id_usuario = " . $leader['idLeader']);
 
-    echo "<tr><td>" . $name . "</td><td>" . $txt['pos_' . $leader['position']] . "</td></tr>";
+    echo "<tr><td><a href='../perfil/" . $leader['idLeader'] . "'>" . $name . "</a></td><td>" . $txt['pos_' . $leader['position']] . "</td></tr>";
 }
 
 echo "</table>";
@@ -35,16 +39,38 @@ echo "</table>";
 echo "<h3>Dineros</h3>";
 
 echo "<table><tr><th>Moneda</th><th>Cantidad</th></tr>";
-echo sql("SELECT Gold FROM money_pais WHERE idcountry ='" . $id_pais . "'") . " " . $txt['gold'] . "<br/>";
+$gold = sql("SELECT Gold FROM money_pais WHERE idcountry ='" . $id_pais . "'");
+
+echo "<tr><td>" . $txt['gold'] . "</td><td>" . $gold . "</td></tr>";
 $sql = sql("SELECT * FROM money_pais WHERE idcountry = " . $id_pais);
 
-unset($sql['idcountry'],$sql['Gold']);
+unset($sql['idcountry'], $sql['Gold']);
 arsort($sql);
 
 foreach ($sql as $moneda => $valor) {
-    if($valor == 0){continue;}
+    if ($valor == 0) {
+        continue;
+    }
     echo "<tr><td>" . $moneda . "</td><td>" . $valor . "</td></tr>";
 }
 
 echo "</table>";
+
+echo "<h3>Regiones</h3>";
+
+$regiones = $pais->list_regions($id_pais);
+
+if ($regiones == false) {
+    echo $txt['no_regions'];
+} else {
+
+    echo "<table><tr><th>Nombre</th></tr>";
+
+    foreach ($regiones as $region) {
+
+        echo "<tr><td><a href='../region/" . $region['idregion'] . "'>" . $region['name'] . "</a></td></tr>";
+    }
+
+    echo "</table>";
+}
 ?>
