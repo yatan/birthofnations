0150ca7c29f8dbc1dca73a1e933fb6e3fb9f0e4d
airumas@gmail.com <airumas@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2011-08-08 23:22:12 +0000

    Viaje basico

---
+++ trunk/usuarios/viajar.php
@@ -0,0 +1,6 @@
+<?
+
+echo "Aqui va el metodo de seleccion de regiones magico de yatan que no se hacer, asi que pongo un link y luego le quitais la variable del principio";
+echo"<br><a href='../viaje.php'>Mueveme</a>";
+
+?>
