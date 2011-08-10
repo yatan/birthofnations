3b6a78f823910b826a13dc04f3198106f8343e8e
airumas@gmail.com <airumas@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2011-08-10 23:56:33 +0000

    typo4

--- politico/lista_candidatos.php
+++ politico/lista_candidatos.php
@@ -8,7 +8,7 @@ $candidatos = sql2("SELECT * FROM candidatos_elecciones WHERE id_votacion = " . 
 foreach($candidatos as $cand){
     echo "Candidato: " . id2nick($cand['id_candidato']) . "<br> Votos: " . $cand['votos'];
     
-    if(puedo_votar($_SESSION['id_usuario'],$votacion['tipo_votacion'],$votacion['tipo_votacion']) == true){
+    if(puedo_votar($_SESSION['id_usuario'],$votacion['tipo_votacion'],$_GET['id']) == true){
         echo '<a href="votar.php?idv='.$_GET['id'].'&vot='.$cand['id_candidato'].'">Vota</a><br>';
     }
 }
