ab3d6d9baa97f1cf637b1a50f0cc3b4bd5f5c305
yatan87@gmail.com <yatan87@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2011-08-14 16:27:38 +0000

    algo de interfaz cutre en guerras, aun mas cutre

--- trunk/militar/historial.php
+++ trunk/militar/historial.php
@@ -6,9 +6,9 @@
   width:90px;
   height:90px;
   margin:5px;
+  display: none;
 }
 </style>
-
 <?
  $n = 0;
 $id_guerra = $_GET['id_guerra'];
@@ -16,14 +16,24 @@ include_once($_SERVER['DOCUMENT_ROOT']."/include/funciones.php");
 $ataques = sql("SELECT id_usuario, hit FROM log_hits WHERE id_guerra='$id_guerra' ORDER BY id_hit DESC LIMIT 5");
 foreach ($ataques as $ataque)
     {
-$avatar=sql("SELECT avatar FROM usuarios WHERE id_usuario='".$ataque['id_usuario']."'");
-    $x = $n*100;
-    echo "<div id ='$n' class='tio'></div>";
-        echo "<script>";
-        echo "avatar='<img width=32 height=32 src=$avatar/>';";
-        echo "$('#$n').append('".id2nick($ataque['id_usuario'])." Daño: ".$ataque['hit']."'+avatar);";
-        echo "$('#$n').animate({'left': '+=".$x."px'}, {duration:2000});";
-        echo "</script>";
+    $avatar=sql("SELECT avatar FROM usuarios WHERE id_usuario='".$ataque['id_usuario']."'");
+    $x = $n*75;
+    $y = $x * 1000;
+        ?>
+        <div id ='<? echo $n; ?>' class='tio' style="left:<? echo $x; ?>;"></div>
+        <script>
+                function heroi(nick, hit, avatar)
+                {
+                    $("#<? echo $n; ?>").append(nick+' Daño:'+hit, avatar);
+                    $("#<? echo $n; ?>").animate({"left": "+=50", "opacity": 1},{duration: <? echo $x*10;?>});
+                    $("#<? echo $n; ?>").show("slow");
+                }
+            avatar='<img width=32 height=32 src=<? echo $avatar; ?>/>';
+            setTimeout(heroi('<? echo id2nick($ataque['id_usuario'])."','".$ataque['id_usuario'];?>',avatar),<? echo $y; ?>);
+        </script>
+        
+
+        <?
     $n++;
         
     }
