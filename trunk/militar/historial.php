4a763bc12f14882cd3f598d05d150156b9d27fd2
yatan87@gmail.com <yatan87@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2011-08-14 15:02:15 +0000

    algo de interfaz cutre en guerras

--- trunk/militar/historial.php
+++ trunk/militar/historial.php
@@ -1,9 +1,34 @@
+<style>
+.tio {
+  position:absolute;
+  background-color:#cba;
+  left:50px;
+  width:90px;
+  height:90px;
+  margin:5px;
+}
+</style>
+
 <?
+ $n = 0;
 $id_guerra = $_GET['id_guerra'];
 include_once($_SERVER['DOCUMENT_ROOT']."/include/funciones.php");
-$ataques = sql("SELECT id_usuario, hit FROM log_hits WHERE id_guerra='$id_guerra' ORDER BY id_hit DESC LIMIT 10");
+$ataques = sql("SELECT id_usuario, hit FROM log_hits WHERE id_guerra='$id_guerra' ORDER BY id_hit DESC LIMIT 5");
 foreach ($ataques as $ataque)
     {
-    echo"<p>".$ataque['id_usuario']." Daño: ".$ataque['hit'];
+$avatar=sql("SELECT avatar FROM usuarios WHERE id_usuario='".$ataque['id_usuario']."'");
+    $x = $n*100;
+    echo "<div id ='$n' class='tio'></div>";
+        echo "<script>";
+        echo "avatar='<img width=32 height=32 src=$avatar/>';";
+        echo "$('#$n').append('".id2nick($ataque['id_usuario'])." Daño: ".$ataque['hit']."'+avatar);";
+        echo "$('#$n').animate({'left': '+=".$x."px'}, {duration:2000});";
+        echo "</script>";
+    $n++;
+        
     }
 ?>
+
+
+
+
