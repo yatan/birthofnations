03269499b180e04974b1a42ba3c8204327678010
airumas@gmail.com <airumas@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2011-08-20 17:14:33 +0000

    Perdon, piñazo modular

--- include/reset_diario.php
+++ include/reset_diario.php
@@ -134,7 +134,9 @@ $DA = sql("SELECT day FROM settings");
 $sql = sql2("SELECT id_partido, frec_elecciones, dia_elecciones FROM partidos");
 
 foreach ($sql as $party) {
-    if ($DA % $party['frec_elecciones'] == $party['dia_elecciones']-2) {//2 dias antes de las elecciones, abrimos la votacion
+    $mod = $DA % $party['frec_elecciones']; //Modulo del dia
+    if($mod < 0){$mod += $party['frec_elecciones'];} // Si es negativo lo subimos
+    if ($mod == $party['dia_elecciones']-2) {//2 dias antes de las elecciones, abrimos la votacion
         {
         $time = time();
         $time2 = $time +86400;
