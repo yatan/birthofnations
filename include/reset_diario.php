92b02e2ca2ecaa54e2a3a5e95b63130524bf3f3b
airumas@gmail.com <airumas@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2011-08-30 15:00:34 +0000

    Si no se presenta nadie tb se marca como resuleta la votacion de PP

--- include/reset_diario.php
+++ include/reset_diario.php
@@ -3,123 +3,100 @@
 //Archivo de reset diario
 
 include_once("config.php");
-function sql_error($sql)
-{
-	
-	$result = mysql_query($sql);
 
-    if ($result == false)
-    {
+function sql_error($sql) {
+
+    $result = mysql_query($sql);
+
+    if ($result == false) {
         //error_log("SQL error: ".mysql_error()."\n\nOriginal query: $sql\n");
         // Remove following line from production servers 
-        die("SQL error: ".mysql_error()."\b<br>\n<br>Original query: $sql \n<br>\n<br>");
+        die("SQL error: " . mysql_error() . "\b<br>\n<br>Original query: $sql \n<br>\n<br>");
     }
-	return $result;
-	
+    return $result;
 }
 
+function sql_data($result) {
 
+    //$result = sql_error($sql);
 
-function sql_data($result)
-{
-	
-	//$result = sql_error($sql);
-	
-	if($lst = mysql_fetch_assoc($result))
-    {
+    if ($lst = mysql_fetch_assoc($result)) {
         mysql_free_result($result);
         return $lst;
     }
-	    return false;
-	
+    return false;
 }
 
-function sql($sql)
-{
-	
-	$result = sql_error($sql);
-	
-        //Si no devuelve nada sale de la funcion
-        if($result==1)
-            return true;
+function sql($sql) {
 
-	if (mysql_num_rows($result) == 1)	
-	{
-		if (mysql_num_fields($result) == 1)
-			{
-				
-			$dato = mysql_fetch_row($result);
-			return $dato[0];
-			
-			}
-		else
-	$table = sql_data($result);
-	}
-	else
-	{
-    
-    $table = array();
-	 if (mysql_num_rows($result) > 0)
-    {
-        $i = 0;
-        while($table[$i] = mysql_fetch_assoc($result)) 
-            $i++;
-        unset($table[$i]);                                                                                  
-    }                                                                                                                                     
-    mysql_free_result($result);
-	}
+    $result = sql_error($sql);
+
+    //Si no devuelve nada sale de la funcion
+    if ($result == 1)
+        return true;
+
+    if (mysql_num_rows($result) == 1) {
+        if (mysql_num_fields($result) == 1) {
+
+            $dato = mysql_fetch_row($result);
+            return $dato[0];
+        }
+        else
+            $table = sql_data($result);
+    }
+    else {
+
+        $table = array();
+        if (mysql_num_rows($result) > 0) {
+            $i = 0;
+            while ($table[$i] = mysql_fetch_assoc($result))
+                $i++;
+            unset($table[$i]);
+        }
+        mysql_free_result($result);
+    }
     return $table;
-	}
+}
 
+function sql2($sql) {
 
-        
-        function sql2($sql)
-{
-	
-	$result = sql_error($sql);
-	
-        //Si no devuelve nada sale de la funcion
-        if($result==1)
-            return true;
+    $result = sql_error($sql);
 
-	if (mysql_num_rows($result) == 1)	
-	{
-		if (mysql_num_fields($result) == 1)
-			{
-				
-			$dato = mysql_fetch_row($result);
-			return $dato[0];
-			
-			}
-		else{
-                    $table = array();
-                    $table[0] = sql_data($result);
-                }
-	
-	}
-	else
-	{
-    
-    $table = array();
-	 if (mysql_num_rows($result) > 0)
-    {
-        $i = 0;
-        while($table[$i] = mysql_fetch_assoc($result)) 
-            $i++;
-        unset($table[$i]);                                                                                  
-    }                                                                                                                                     
-    mysql_free_result($result);
-	}
+    //Si no devuelve nada sale de la funcion
+    if ($result == 1)
+        return true;
+
+    if (mysql_num_rows($result) == 1) {
+        if (mysql_num_fields($result) == 1) {
+
+            $dato = mysql_fetch_row($result);
+            return $dato[0];
+        } else {
+            $table = array();
+            $table[0] = sql_data($result);
+        }
+    } else {
+
+        $table = array();
+        if (mysql_num_rows($result) > 0) {
+            $i = 0;
+            while ($table[$i] = mysql_fetch_assoc($result))
+                $i++;
+            unset($table[$i]);
+        }
+        mysql_free_result($result);
+    }
     return $table;
-	}
+}
+
 //Seguridad       
-if(!isset($_SERVER['argv']))
+if (!isset($_SERVER['argv']))
     die("Error");
 
 $pin = $_SERVER['argv'][1];
 $pin2 = sql("SELECT pin FROM settings");
 
-if($pin != $pin2)
+if ($pin != $pin2)
     die("Error");
 
 //Economico
@@ -135,14 +112,14 @@ $sql = sql2("SELECT id_partido, frec_elecciones, dia_elecciones FROM partidos");
 
 foreach ($sql as $party) {
     $mod = $DA % $party['frec_elecciones']; //Modulo del dia
-    if($mod < 0){$mod += $party['frec_elecciones'];} // Si es negativo lo subimos
-    if ($mod == $party['dia_elecciones']-2) {//2 dias antes de las elecciones, abrimos la votacion
-        {
-        $time = time();
-        $time2 = $time +86400 - 100; //quitamos unos cuantos, por si da problemas al 
-        sql("INSERT INTO votaciones(tipo_votacion,fin,comienzo,param1) VALUES ('1','" . $time2 . "',' " . $time . "','" . $party['id_partido'] . "')");
+    if ($mod < 0) {
+        $mod += $party['frec_elecciones'];
+    } // Si es negativo lo subimos
+    if ($mod == $party['dia_elecciones'] - 2) {//2 dias antes de las elecciones, abrimos la votacion {
+            $time = time();
+            $time2 = $time + 86400 - 100; //quitamos unos cuantos, por si da problemas al 
+            sql("INSERT INTO votaciones(tipo_votacion,fin,comienzo,param1) VALUES ('1','" . $time2 . "',' " . $time . "','" . $party['id_partido'] . "')");
         }
-        
     }
 }
 
@@ -166,10 +143,11 @@ foreach ($sql as $votacion) {
 
         //Cambiar las casilla de lider
         sql("UPDATE partidos SET id_lider = " . $winner['id'] . " WHERE id_partido = " . $votacion['param1']);
-        sql("UPDATE votaciones SET solved = 1 WHERE id_votacion = " . $votacion['id_votacion']);
     } else {//Si no hay candidatos
         //Todo sigue igual
     }
+    //marcar como resuelta
+    sql("UPDATE votaciones SET solved = 1 WHERE id_votacion = " . $votacion['id_votacion']);
 }
 
 echo "Cron realizado correctamente";
