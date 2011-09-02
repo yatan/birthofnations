b925c47c7c9846488fbadcd6ba8bba883846a1c5
yatan87@gmail.com <yatan87@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2011-09-02 17:14:14 +0000

    Seleccion de region al nacer Añadidos nombres items en traducciones

--- trunk/login/primer_login.php
+++ trunk/login/primer_login.php
@@ -14,28 +14,33 @@ display: none;
 }
 </style>
 
-<script>
+<script language="javascript">
 	$(function() {
 		$( "#dialog" ).dialog({draggable: false, resizable: false});
 	});
+        
+function cambiar_pais(arg) {
+    $("#regi").load("/usuarios/v_lista_regiones.php", {id_pais: arg});	
+}
 </script>
 
 <div id="dialog" title="Bienvenido a Birth of Nations">
 	<p>Este es tu primer login al juego, selecciona tu destinación...</br>Blah, blah, blah</p>
         <p>Seleccionar pais destino:</p>
         <form id="viaje">
-            <select id="pais" name="pais">
+            <select id="pais" name="pais" onchange="cambiar_pais(this.value)">
+                <option value="0">-</option>
                 <?
-                $sql = sql("SELECT idcountry, name FROM country");
+                $sql = sql("SELECT idcountry, name FROM country WHERE EXISTS ( SELECT * FROM region WHERE region.idcountry = country.idcountry )");
                 foreach ($sql as $pais) {
                     echo "<option value='".$pais['idcountry']."'>".$pais['name']."</option>";
                 }
                 ?>
                 </select>
+            <a id="regi"><p></p></a><br>
             <input type="hidden" name="tokken" value="<? echo base64_encode($_SESSION['id_usuario']); ?>"/>
             <input type="button" value="Viajar" id="enviar"/>
         </form>
-        <p>Regiones aun no implementadas</p>
 </div>
 
 <script>
--- trunk/login/primer_viaje.php
+++ trunk/login/primer_viaje.php
@@ -10,13 +10,18 @@ include_once($_SERVER['DOCUMENT_ROOT']."/include/funciones.php");
 $id_usuario = base64_decode($_POST['tokken']);
 $pais_destino = $_POST['pais'];
 
+if(!isset($_POST['region']) || $_POST=="0")
+    die("Selecciona una region!");
+else
+    $region_destino = $_POST['region'];
+
 $pais_actual = sql("SELECT id_pais FROM usuarios WHERE id_usuario='$id_usuario'");
 
 if($pais_actual != null)
     die("Ya estas en un pais !!!");
 
 //Se actualiza el pais
-$sql = sql("UPDATE usuarios SET id_pais='$pais_destino', id_nacionalidad='$pais_destino' WHERE id_usuario='$id_usuario'");
+$sql = sql("UPDATE usuarios SET id_pais='$pais_destino', id_nacionalidad='$pais_destino', id_region='$region_destino' WHERE id_usuario='$id_usuario'");
 
 if($sql==false)
     die("error");
