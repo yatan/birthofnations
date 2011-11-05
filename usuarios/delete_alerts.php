06c13c16a6f4bff4c2c42f9e4b97483197ee2177
yatan87@gmail.com <yatan@users.noreply.github.com>
2011-11-05 00:06:01 +0000

    Algo en la pagina de alertas Posibilidad de borrar las alertas

---
+++ usuarios/delete_alerts.php
@@ -0,0 +1,24 @@
+<?
+
+/*
+ * Hay que añadir la comprobacion de que las ids que llegan por
+ * POST pertenecen a su dueño
+ */
+
+include_once($_SERVER['DOCUMENT_ROOT'] . "/include/funciones.php");
+
+//session_start();
+//echo "nick: ".$_SESSION['id_usuario'];
+
+$contador = 0;
+
+
+if (isset($_POST['alertas']))
+{
+foreach ($_POST['alertas'] as $alerta) {
+    sql("DELETE FROM alertas WHERE id_alerta='$alerta'");
+    $contador += 1;
+}
+}
+echo "Alertas eliminadas: $contador";
+?>
