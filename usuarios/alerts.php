213e3890bb6970b630d1aabdafeb490ff39cfefd
yatan87@gmail.com <yatan@users.noreply.github.com>
2011-11-09 00:55:13 +0000

    mejoras en alertas: deteccion de 2 tipos de alertas: amigos, nuevos empleados Al borrar una alerta se modifica el tipo a 0 funcion nueva: id2empresa($id) : para saber el nombre de una empresa :D

--- usuarios/alerts.php
+++ usuarios/alerts.php
@@ -10,17 +10,36 @@ echo "<h1>Alertas</h1>";
 
 $alertas = sql2("SELECT * FROM alertas WHERE id_receptor='".$_SESSION['id_usuario']."' ORDER BY id_alerta DESC");
 
-echo "<form id='borrar_alertas'>";
+echo "<form id='borrar_alertas'><table style='text-align: center; width: 600px; margin-left: auto; margin-right: auto; padding: 5px;'><tr><th style='width: 100px'>Tipo</th><th style='width: 310px'>Alerta</th><th>Eliminar</th></tr>";
 
 foreach ($alertas as $alerta) {
-    echo id2nick($alerta['id_emisor'])." quiere ser tu amigo. <a href='#'>Añadir a amigos</a> <input type='checkbox' name='alertas[]' value='".$alerta['id_alerta']."'/>";
-    echo "<hr>";
+    
+    switch ($alerta['tipo']) {
+        case "1":
+            echo "<tr><td>Amigo</td><td>";
+            echo id2nick($alerta['id_emisor'])." quiere ser tu amigo. <a href='#'>Añadir a amigos</a></td>";
+            echo "<td><input type='checkbox' name='alertas[]' value='".$alerta['id_alerta']."'/></td></tr>";
+            
+            break;
+        case "2":
+            echo "<tr><td>Trabajador</td><td>";
+            echo id2nick($alerta['id_emisor'])." ahora trabaja en <b>".id2empresa($alerta['r1'])."</b></td>";
+            echo "<td><input type='checkbox' name='alertas[]' value='".$alerta['id_alerta']."'/></td></tr>";
+            
+            break;           
+
+
+        default:
+            break;
+    }
+    
+
 }
 
 
 ?>
 
-
+</table>
 </form>
 
 
