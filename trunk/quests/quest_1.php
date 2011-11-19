f48ca0bdc5d871928fd52a4c066714bf2ec991d8
yatan87@gmail.com <yatan87@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2011-11-19 18:08:53 +0000

    quest 1 (no funcional) +Modificaciones Eliminado link en status bar que no servia para nada *Reparado bug en seleccionar pais (.htaccess)

--- trunk/quests/quest_1.php
+++ trunk/quests/quest_1.php
@@ -7,6 +7,9 @@
  * Test de quest
  */
 
+include_once($_SERVER['DOCUMENT_ROOT']."/include/funciones.php");
+include("../index_head.php");
+
 class quest_1
 {
     
@@ -66,29 +69,33 @@ class quest_1
         echo "Has terminado la quest !<br>";
         echo "Obtienes 10 de EXP y 2 pociones<br>";
         //Aqui codigo para añadir items
+        sql("UPDATE usuarios SET exp = exp + {$this->exp_recompensa} WHERE id_usuario='1'");
     }
     
+    function info()
+    {
+       echo $this->nombre_quest ."<br>";
+       echo $this->texto_quest ."<br>";
+       echo $this->recompensa ."<br>";
+    }
     
     function guardar()
     {
        $s = serialize($this);
-       $fp = fopen("quest_1.txt", "w");
-       fwrite($fp, $s);
-       fclose($fp);        
+       sql("UPDATE quests SET quest_1='$s' WHERE id_usuario='1'");
     }
     
     function cargar()
     {
-        $archivo = @file("quest_1.txt");
+        $archivo = sql("SELECT quest_1 FROM quests WHERE id_usuario='1'");
         if($archivo == false)
         {
             $this->lobos_muertos=0;
         }
         else
         {
-            $s = implode("", $archivo);
-            $a = unserialize($s);
-
+            
+            $a = unserialize($archivo);
 
             $this->lobos_muertos = $a->lobos_muertos;
 
@@ -106,13 +113,39 @@ class quest_1
     
 }
 
-$mision = new quest_1("1");
-
-$mision ->matar_lobo();
-$mision ->matar_lobo();
+$mision = new quest_1();
+/*
 $mision ->matar_lobo();
 
+$mision ->matar_lobo();
 
+*/
 
 
 ?>
+
+<div id="juego">
+<div id="mapa" style="height: 600px; width: 600px; background-color: green; margin-top: 50px;">
+    <?
+    for($x=0;$x<5;$x++)
+    {
+        for($y=0;$y<5;$y++)
+        {
+            echo "<img src='http://www.dibujosparacolorear24.com/flash/es/thumbs/54423.png'/>";
+        }
+        echo "<br>";
+    }
+    ?>
+</div>
+
+<div id="info" style="position:absolute;top:50px; left: 30px; height: 350px; width: 280px; background-color: #fff; margin-left: 900px; margin-top: 100px;">
+    <center><h3>Log</h3></center>
+    <ul>
+<?
+$mision->info();
+$mision ->matar_lobo();
+?>
+    </ul>
+</div>
+    
+</div>
