dae817ff58b320cddcd11181f6f895ed1389d4aa
yatan87@gmail.com <yatan87@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2011-12-07 22:24:06 +0000

    *Corregidas varias cosas en status_bar

--- trunk/status.php
+++ trunk/status.php
@@ -5,12 +5,16 @@
  * and open the template in the editor.
  */
 $dia = sql("SELECT day FROM settings");
-$nick = sql("SELECT nick FROM usuarios WHERE id_usuario='".$_SESSION['id_usuario']."'");
+$datos = sql("SELECT nick, status, salud FROM usuarios WHERE id_usuario='".$_SESSION['id_usuario']."'");
 $gold = sql("SELECT gold FROM money WHERE id_usuario='".$_SESSION['id_usuario']."'");
 $mensajes = sql("SELECT COUNT(*) FROM messages WHERE id_receptor='".$_SESSION['id_usuario']."' AND leido='0' AND deleted='0'");
 $alertas = sql("SELECT COUNT(*) FROM alertas WHERE id_receptor='".$_SESSION['id_usuario']."' AND leido='0'");
 $pais = sql("SELECT name, url_bandera FROM country WHERE idcountry IN (SELECT id_nacionalidad FROM usuarios WHERE id_usuario='".$_SESSION['id_usuario']."')");
-$estado = sql("SELECT status FROM usuarios WHERE id_usuario='".$_SESSION['id_usuario']."'");
+
+
+$nick = $datos['nick'];
+$estado = $datos['status'];
+$salud = $datos['salud'];
 
 if($estado != null)
 {
@@ -43,7 +47,7 @@ echo "$i_estado - <a href='/".$_GET['lang']."/perfil/".$_SESSION['id_usuario']."
 ." - "
 ."<img alt='bandera' title='".$pais['name']."' src='".$pais['url_bandera']."'>"
 ." - "
-."<img alt='vida'  src='/images/status_bar/life.gif'> 100"
+."<img alt='vida'  src='/images/status_bar/life.gif'> $salud"
 ." - "
 ."<img alt='gold' src='/images/status_bar/gold.gif'> $gold golds"
 ." - ";
