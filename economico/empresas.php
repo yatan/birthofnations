5585bcbf4d7d8f4a6f56a9396ffbe40a4df0727c
yatan87@gmail.com <yatan@users.noreply.github.com>
2011-12-09 02:08:03 +0000

    *Mejorada un poco la vision de la lista de empresas con su tipo

--- economico/empresas.php
+++ economico/empresas.php
@@ -11,19 +11,21 @@ include_once($_SERVER['DOCUMENT_ROOT']."/include/funciones.php");
 
 echo"<h1>Mis empresas</h1>";
 
-$cantidad_empresas = sql("SELECT COUNT(id_empresa) FROM empresas WHERE id_propietario='".$_SESSION['id_usuario']."'");
+//$cantidad_empresas = sql("SELECT COUNT(id_empresa) FROM empresas WHERE id_propietario='".$_SESSION['id_usuario']."'");
 
 $sql = sql("SELECT * FROM empresas WHERE id_propietario='".$_SESSION['id_usuario']."'");
 
-if($cantidad_empresas == 1)
-echo "<a href='/".$_GET['lang']."/empresa/".$sql['id_empresa']."'>".$sql['nombre_empresa']."</a><br/>";
-else {
-    
+//if($cantidad_empresas == 1)
+//echo "<a href='/".$_GET['lang']."/empresa/".$sql['id_empresa']."'>".$sql['nombre_empresa']."</a><br/>";
+//else {
+echo "<table><tr><td>Tipo</td><td>Nombre</td></tr>";
+
 foreach ($sql as $empresa) {
-    echo "<a href='/".$_GET['lang']."/empresa/".$empresa['id_empresa']."'>".$empresa['nombre_empresa']."</a><br/>";
+    echo "<tr><td>".  id2item($empresa['tipo'])."</td><td><a href='/".$_GET['lang']."/empresa/".$empresa['id_empresa']."'>".$empresa['nombre_empresa']."</a></td></tr>";
     }
 
-}
+//}
+echo "</table>";
 ?>
 <br/>
 <a style=" background-color:#1E679A ; border: 1px solid #1E679A;" href="<? echo "/".$_GET['lang']."/crear_empresa"; ?>"><font color="#FFFFFF" face="arial, verdana, helvetica">Crear Empresa</font></a>
