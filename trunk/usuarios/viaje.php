f75edd85a2e64442b0b34816acf46c32914c812f
yatan87@gmail.com <yatan87@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2011-12-16 17:58:45 +0000

    *Viajes y varios fallos reparados

--- trunk/usuarios/viaje.php
+++ trunk/usuarios/viaje.php
@@ -12,7 +12,7 @@ $destino = (int)$_POST['region'];
 $origen = sql("SELECT salud, id_region FROM usuarios WHERE id_usuario = ". $_SESSION['id_usuario']);
 
 //Comprobar que no es la misma
-if($destino == $origen['id_region']){die("Para dar vueltas en circulo te hacesd una noria");}
+if($destino == $origen['id_region']){die("Para dar vueltas en circulo te haces una noria");}
 
 //Comprobamos la distancia entre las dos regiones, a ver si me acuerdo de como era:
 
@@ -31,9 +31,11 @@ $tickets = sql("SELECT transporte FROM inventario WHERE id_usuario = " . $_SESSI
 
 if($tickets >= $distancia && $origen['salud'] > 0 ){//Si puede viajar
     //Quitar items y mierdas y actualizar el pais
-    sql("UPDATE usuarios SET salud = salud - 1, id_region = ". $destino . ", id_pais = ".$region_destino->owner_id()." WHERE id_usuario = " . $_SESSION['id_usuario']);
+    sql("UPDATE usuarios SET salud = salud - 1, id_region = ". $destino . " WHERE id_usuario = " . $_SESSION['id_usuario']);
     sql("UPDATE inventario SET transporte = transporte - " . $distancia . " WHERE id_usuario = " . $_SESSION['id_usuario']);
-    echo"Has viajado correctamente";
+    $tiempo = time()+(60*$distancia);
+    sql("INSERT INTO viajes(id_usuario, hora_final) VALUES('" . $_SESSION['id_usuario'] . "','$tiempo')");
+    echo"Empieza el viaje, durara $distancia minutos";
 }else{//Si no tiene objetos
     echo "No cumples los requisitos necesarios para viajar, revisa tu existencia, mortal.";
     
