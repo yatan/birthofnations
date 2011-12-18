00406690f4ad00eaf02acaebf18153391fed727e
yatan87@gmail.com <yatan87@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2011-12-18 16:11:15 +0000

    *Reparado bug 13

--- trunk/login/primer_viaje.php
+++ trunk/login/primer_viaje.php
@@ -15,13 +15,13 @@ if(!isset($_POST['region']) || $_POST=="0")
 else
     $region_destino = $_POST['region'];
 
-$pais_actual = sql("SELECT id_pais FROM usuarios WHERE id_usuario='$id_usuario'");
+$pais_actual = sql("SELECT id_region FROM usuarios WHERE id_usuario='$id_usuario'");
 
 if($pais_actual != null)
     die("Ya estas en un pais !!!");
 
 //Se actualiza el pais
-$sql = sql("UPDATE usuarios SET id_pais='$pais_destino', id_nacionalidad='$pais_destino', id_region='$region_destino' WHERE id_usuario='$id_usuario'");
+$sql = sql("UPDATE usuarios SET id_nacionalidad='$pais_destino', id_region='$region_destino' WHERE id_usuario='$id_usuario'");
 
 if($sql==false)
     die("error");
