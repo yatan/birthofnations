a9c7e85552581b8a8f792a3addf7f284e16296c7
airumas@gmail.com <airumas@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2011-12-28 20:45:10 +0000

    Apaño bug #14

--- usuarios/viaje.php
+++ usuarios/viaje.php
@@ -4,40 +4,46 @@ include_once($_SERVER['DOCUMENT_ROOT'] . "/politico/objeto_region.php");
 include_once($_SERVER['DOCUMENT_ROOT'] . "/include/funciones.php");
 include_once($_SERVER['DOCUMENT_ROOT'] . "/include/config_variables.php");
 
-//Esta es la variable que llega como destino:
+if (isset($_POST['region']) && $_POST['region'] != "" && strlen($_POST['region']) > 0) {
 
-$destino = (int)$_POST['region'];
+//Esta es la variable que llega como destino
+
+    $destino = (int) $_POST['region'];
 
 //Sacamos la region origen
 
-$origen = sql("SELECT salud, id_region FROM usuarios WHERE id_usuario = ". $_SESSION['id_usuario']);
+    $origen = sql("SELECT salud, id_region FROM usuarios WHERE id_usuario = " . $_SESSION['id_usuario']);
 
 //Comprobar que no es la misma
-if($destino == $origen['id_region']){die("Para dar vueltas en circulo te haces una noria");}
+    if ($destino == $origen['id_region']) {
+        die("Para dar vueltas en circulo te haces una noria");
+    }
 
 //Comprobamos la distancia entre las dos regiones, a ver si me acuerdo de como era:
 
-$region_destino = new region($destino);
-$region_origen = new region($origen['id_region']);
+    $region_destino = new region($destino);
+    $region_origen = new region($origen['id_region']);
 
-$ruta = $region_origen->distance_to($destino);
+    $ruta = $region_origen->distance_to($destino);
 
 // [1][0] - [1][1] es la ruta
-$distancia = (int)$ruta[$destino]['distance'];
+    $distancia = (int) $ruta[$destino]['distance'];
 
 //Ahora comprobamos que tiene suficientes objetos necesarios para viajar, cuales sean, pa probar sugus
 
-$tickets = sql("SELECT transporte FROM inventario WHERE id_usuario = " . $_SESSION['id_usuario']); //Comentario temporal
+    $tickets = sql("SELECT transporte FROM inventario WHERE id_usuario = " . $_SESSION['id_usuario']); //Comentario temporal
 
-if($tickets >= $distancia && $origen['salud'] >= $min_travel_health ){//Si puede viajar
-    //Quitar items y mierdas y actualizar el pais
-    sql("UPDATE usuarios SET salud = salud - 1, id_region = ". $destino . " WHERE id_usuario = " . $_SESSION['id_usuario']);
-    sql("UPDATE inventario SET transporte = transporte - " . $distancia . " WHERE id_usuario = " . $_SESSION['id_usuario']);
-    $tiempo = time()+(60*$distancia);
-    sql("INSERT INTO viajes(id_usuario, hora_final) VALUES('" . $_SESSION['id_usuario'] . "','$tiempo')");
-    echo"Empieza el viaje, durara $distancia minutos";
-}else{//Si no tiene objetos
-    echo $txt['cant_travel'];
+    if ($tickets >= $distancia && $origen['salud'] >= $min_travel_health) {//Si puede viajar
+        //Quitar items y mierdas y actualizar el pais
+        sql("UPDATE usuarios SET salud = salud - 1, id_region = " . $destino . " WHERE id_usuario = " . $_SESSION['id_usuario']);
+        sql("UPDATE inventario SET transporte = transporte - " . $distancia . " WHERE id_usuario = " . $_SESSION['id_usuario']);
+        $tiempo = time() + (60 * $distancia);
+        sql("INSERT INTO viajes(id_usuario, hora_final) VALUES('" . $_SESSION['id_usuario'] . "','$tiempo')");
+        echo"Empieza el viaje, durara $distancia minutos";
+    } else {//Si no tiene objetos
+        echo $txt['cant_travel'];
+    }
+} else {
+    die("So quick...");
 }
-
 ?>
