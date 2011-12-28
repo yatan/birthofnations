c0497fc49d399b51b4effb9ecea0829cd3a09d8b
Etchner@gmail.com <Etchner@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2011-12-28 22:34:21 +0000

    i18n primera fase. A ver si hay algo de suerte

--- include/config_variables.php
+++ include/config_variables.php
@@ -5,6 +5,10 @@
  * (por ejemplo puntos de experiencia necesarios para subir de nivel, limite de desarrollo tecnológico...
  */
 
+//sistema
+
+$lenguaje_principal="es";
+
 //Modulo general
 
 $friendship_expire = 86400; //Tiempo que debe transcurrir paara que puedas reenviar una peticion de amistad
