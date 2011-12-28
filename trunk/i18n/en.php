27281427bced65cba7e2c2831b5cfec50735c57b
Etchner@gmail.com <Etchner@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2011-12-28 22:35:14 +0000

    i18n primera fase. A ver si hay algo de suerte

---
+++ trunk/i18n/en.php
@@ -0,0 +1,61 @@
+<?php
+
+$i18n_array['activacion_titulo'] = 'Birth of Nations account activation';
+$i18n_array['activacion_mensaje_titulo'] = "Welcome to Birth of Nations; ";
+$i18n_array['activacion_mensaje_cuerpo'] = "In order to complete your register; you need to activate your account.
+       Please; go to the following url to get your account activated";
+$i18n_array['activacion_mensaje_nick'] = "Registered user: ";
+$i18n_array['activacion_mensaje_mensaje1'] = "If you haven't register an account. Please ignore this mail";
+$i18n_array['activacion_mensaje_firma'] = "BirthOfNations staff";   
+    
+    
+    
+    
+$i18n_array['signup_title'] = "Register form"    ;
+$i18n_array['signup_username'] = "Nickname "    ;
+$i18n_array['signup_username_info'] = "characters from A to z; you type more than 5 characters "    ;
+$i18n_array['signup_password'] = "Please type from 5 characters to 12"    ;
+$i18n_array['signup_password2'] = "This password must be exactly than the above password "    ;
+$i18n_array['signup_mail'] = "email"    ;
+$i18n_array['signup_mail_info'] = 'Please; type a valid mail';
+$i18n_array['signup_submit'] = "Submit"    ;
+$i18n_array['signup_mail_send_form'] = "Register";
+$i18n_array['signup_mail_error'] = "Unable to connect with the mail server. Please try again later";
+    
+    
+ $i18n_array['signup_title'] = "Register form"    ;
+$i18n_array['signup_username'] = "Nickname "    ;
+$i18n_array['signup_username_info'] = "characters from A to z; you type more than 5 characters "    ;
+$i18n_array['signup_password'] = "Please type from 5 characters to 12"    ;
+$i18n_array['signup_password2'] = "This password must be exactly than the above password "    ;
+$i18n_array['signup_mail'] = "email"    ;
+$i18n_array['signup_mail_info'] = 'Please; type a valid mail';
+$i18n_array['signup_submit'] = "Submit"    ;
+    
+$i18n_array['login_texto'] = "Birth of Nations is a free online strategic game. The development of the game is done by The Community and the decisions are taken by the players.";
+$i18n_array['login_nonick'] = "You haven't type a nickname";
+$i18n_array['login_nopass'] = "You haven't type a password";
+$i18n_array['login_nomatch'] = "You have typed an incorrect nick or pass [0x01]" ;
+$i18n_array['login_nomatch2'] = "You have typed an incorrect nick or pass [0x02]";
+$i18n_array['login_recovery'] = "Recovery password"; 
+    
+    
+        //PORTADA
+    
+$i18n_array['periodicos_login'] = 'Newspapers';
+$i18n_array['guerras_login'] = 'Lastest wars';
+    
+    
+        //ERRORES
+    
+$i18n_array['conection_fail_1'] = 'There was an error while connecting to the server. Please try again later';
+    
+    
+//PIE
+
+$i18n_array['foot_status_begin'] = 'There are: ';
+$i18n_array['foot_status_mysql_user'] = "users online ";
+$i18n_array['foot_status_mysql_status'] = "mysql status: ";
+
+
+?>
