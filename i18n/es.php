62a661ccfb35ac4989e8f6d6e52ae0d57cff9a19
Etchner@gmail.com <Etchner@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2011-12-28 22:35:14 +0000

    i18n primera fase. A ver si hay algo de suerte

---
+++ i18n/es.php
@@ -0,0 +1,180 @@
+<?php   
+		//REGISTRO
+
+$i18n_array['signup_title'] = "Formulario de Registro";
+$i18n_array['signup_username'] = "Nombre de usuario ";
+$i18n_array['signup_username_info'] = "Caracteres de A-z; mínimo 5 caracteres ";
+$i18n_array['signup_pass'] = 'Contraseña';
+$i18n_array['signup_pass_info'] = "Mínimo 5 caracteres; máximo 12 caracteres; letras y símbolos";
+$i18n_array['signup_pass2'] = 'Repetir contraseña';
+$i18n_array['signup_pass_info2'] = "Debe ser igual a la anterior ";
+$i18n_array['signup_referer'] = 'Referidor';
+$i18n_array['signup_mail'] = "email";
+$i18n_array['signup_mail_info'] = 'Escribe un email válido por favor';// No se usa??
+$i18n_array['signup_submit'] = "Formulario de Registro";// No se usa??
+$i18n_array['signup_mail_send_form'] = "Enviar formulario";
+$i18n_array['signup_mail_error'] = "Se produjo un error al conectar con el servidor de mail.";
+
+ 		//LOGIN
+
+$i18n_array['login_texto'] = "Birth of Nations es un juego online libre de estrategia política; económica y social. El desarrollo del juego se realiza de forma comunitaria; y sus propios jugadores deciden cual es el rumbo que toma el proyecto.";
+$i18n_array['login_nonick'] = "Falta el usuario";
+$i18n_array['login_nopass'] = "Falta la contraseña";
+$i18n_array['login_nomatch'] = "La combinación de usuario/password no es la correcta [0x01]" ;
+$i18n_array['login_nomatch2'] = "La combinación de usuario/password no es la correcta [0x02]";
+$i18n_array['login_banned'] = "Has sido baneado"; 
+$i18n_array['login_recovery'] = "Recuperar password"; 
+
+		//FIRST LOGIN
+
+$i18n_array['first_login_title'] = "Bienvenido a Birth of Nations";
+$i18n_array['first_login_dialog'] = "Este es tu primer login al juego; selecciona tu destinación...</br>Blah; blah; blah";
+$i18n_array['first_login_travel'] = "Seleccionar pais destino:";
+
+//RECOVERY ACCOUNT
+
+
+$i18n_array['login_recovery_title'] = "Recuperar password";
+$i18n_array['login_recovery_email'] = "E-mail";
+$i18n_array['login_reset_password'] = "Resetear password";
+
+$i18n_array['code_error'] = "Error en el codigo";
+$i18n_array['referer_title'] = "Alguien te ha invitado a Birth of Nations";
+$i18n_array['referer_mail1'] = "Bienvenido<br/>Recibes este correo porque: ";
+$i18n_array['referer_mail2'] = " te ha invitado al juego.<br/>Para aceptar la invitacion; haz click al siguiente enlace; en caso contrario ignora el correo.<br/>";
+$i18n_array['mail_bienvenida1'] = "<p><img src='http://birthofnations.com/images/titulo.png' alt='logo' width='498' height='99' align='middle' />
+                            </p>
+                            <p>&nbsp;</p>
+                            <p>Bienvenido a Birth of Nations tus datos de registro son:</p>
+                            <p>Usuario: ";
+$i18n_array['mail_bienvenida2'] = "</p>
+    <p>Automaticamente se ha creado la cuenta en el foro y la gestion de bugs con los mismos datos que el juego.</p>
+    <p><u><strong>Foro:</strong></u></p>
+    <p> <a href='http://birthofnations.com/forum'>http://birthofnations.com/forum</a></p>
+    <p><strong><u>Reporte de bugs:</u></strong></p>
+    <p> <a href='http://birthofnations.com/bugs/'>http://birthofnations.com/bugs/</a></p>
+    <p><strong><u>Tickets de soporte:</u></strong></p>
+    <p> <a href='http://birthofnations.com/support/'>http://birthofnations.com/support/</a></p>
+    <p>&nbsp;</p>
+    <p>Disfruta del juego</p>
+    <p> <a href='http://birthofnations.com/'>http://birthofnations.com/</a></p>
+    <p>&nbsp;</p>";
+$i18n_array['mail_bienvenida_title'] = "Bienvenido a Birth of Nations";
+
+//Economico
+$i18n_array['Gold'] = 'Gold';
+$i18n_array['Poner_ofertas_trabajo'] = "Poner ofertas de trabajo";
+$i18n_array['empresa_tipo1'] = "Pan";
+$i18n_array['empresa_tipo2'] = "Trigo";
+$i18n_array['empresa_tipo3'] = "Oil";
+$i18n_array['empresa_tipo4'] = "Transporte";
+$i18n_array['no_ofertas_trabajo'] = "No hay ofertas de trabajo";
+
+//Estos 3 deberian desaparecer
+$i18n_array['item1'] = "Sugus";
+$i18n_array['item2'] = "Azucar";
+$i18n_array['item3'] = "Armas";
+$i18n_array['item4'] = "Plumas";
+$i18n_array['item10'] = "Cuchillo";
+
+//Usuarios
+$i18n_array['cant_travel'] = 'No cumples los requisitos necesarios para viajar; revisa tu existencia; mortal.';
+$i18n_array['cant_work'] = 'Necesitas 10 puntos de salud para trabajar.';
+$i18n_array['no_train_health'] = 'Necesitas 10 puntos de salud para entrenar.';
+$i18n_array['no_mensajes'] = 'No tienes mensajes';
+$i18n_array['no_friends'] = "Sin amigos";
+$i18n_array['no_cantidad'] = "Por favor introduce cantidad correcta";
+$i18n_array['operacion_ok'] = "La operación se ha realizado correctamente";
+$i18n_array['me_falta_dinero'] = "No dispongo de tanto dinero";
+$i18n_array['add_friend_head'] = "Peticion de amistad";
+$i18n_array['add_friend'] = "quiere ser tu amigo. �Aceptas?";
+$i18n_array['stat_a'] = "Hambriento";
+$i18n_array['stat_b'] = "Necesita sexo";
+$i18n_array['stat_c'] = "Sobredosis de azucar";
+$i18n_array['stat_S'] = "Suicida";
+$i18n_array['no_stat'] = "Bien";
+
+//Pagina mantenimiento
+$i18n_array['mantenimiento_1'] = "Modo mantenimiento";
+$i18n_array['mantenimiento_2'] = "El juego se encuentra en mantenimiento.";
+$i18n_array['mantenimiento_3'] = "Para visitar el foro use el siguiente link:";
+$i18n_array['mantenimiento_4'] = "Acceder al foro";
+
+//Politico
+$i18n_array['already_voted'] = 'Ya has votado';
+$i18n_array['no_regions'] = "Este pais no posee ninguna region actualmente";
+
+//Menu
+
+$i18n_array['economico'] = "Economico";
+$i18n_array['militar'] = "Militar";
+$i18n_array['mercados'] = "Mercados";
+$i18n_array['pais'] = "Pais";
+$i18n_array['soporte'] = "Soporte";
+$i18n_array['trabajar'] = "Trabajar";
+$i18n_array['mis_empresas'] = "Mis Empresas";
+$i18n_array['perfil'] = "Mi Perfil";
+$i18n_array['entrenamiento'] = "Entrenamiento";
+$i18n_array['guerras'] = "Guerras";
+$i18n_array['mercado_productos'] = "Mercado de productos";
+$i18n_array['mercado_laboral'] = "Mercado laboral";
+$i18n_array['mercado_empresas'] = "Mercado de empresas";
+$i18n_array['mi_pais'] = "Mi País";
+$i18n_array['partidos_politicos'] = "Partidos politicos";
+$i18n_array['ranking'] = "Clasificación";
+$i18n_array['foro'] = "Foro";
+$i18n_array['tickets'] = "Tickets de soporte";
+$i18n_array['bugs'] = "Reporte de bugs";
+$i18n_array['wiki'] = "Wiki";
+//Items
+
+$i18n_array['sugus'] = "Sugus";
+$i18n_array['azucar'] = "Azucar";
+$i18n_array['armas'] = "Armas";
+$i18n_array['hierro'] = "Hierro";
+//Leyes
+
+$i18n_array['law_100'] = 'Cambiar nombre al pais';
+$i18n_array['law_101'] = 'A�adir cargo';
+$i18n_array['law_102'] = 'Eliminar cargo';
+$i18n_array['law_103'] = 'Dar cargo';
+$i18n_array['law_104'] = 'Quitar cargo';
+//Rangos Militares
+
+$i18n_array['rango_0'] = 'Soldado';
+$i18n_array['rango_1'] = 'Tirillas';
+//PORTADA
+
+$i18n_array['periodicos_login'] = 'Periodicos';
+$i18n_array['guerras_login'] = 'Últimas guerras';
+
+//ERRORES
+
+$i18n_array['conection_fail_1'] = 'Hay un fallo en los servidores. Intentalo mas tarde';
+//PIE
+
+$i18n_array['foot_status_begin'] = 'Hay: ';
+$i18n_array['foot_status_mysql_user'] = "usuarios online";
+$i18n_array['foot_status_mysql_status'] = "mysql status: ";
+
+
+//COMPANIES
+$i18n_array['company_error_left_work'] = "Necesitas salir de tu trabajo";
+$i18n_array['company'] = "Empresa:";
+$i18n_array['company_error_not_valid'] = "Empresa no valida";
+$i18n_array['company_ups_something_is_wrong'] = "Algo falla";
+$i18n_array['company_inventory'] = "Inventario de la empresa";
+$i18n_array['use_item'] = "Usar item";
+$i18n_array['sell_stock'] = "Vender stock";
+$i18n_array['sales'] = "Ventas";
+$i18n_array['button_change_salary'] = "Cambiar salario";
+$i18n_array['company_money'] = "Dinero";
+$i18n_array['cantidad'] = "Cantidad";
+$i18n_array['coin'] = "Moneda";
+$i18n_array['ingresar'] = "Ingresar";
+$i18n_array['retirar'] = "Retirar";
+$i18n_array['vender_empresa'] = "Vender empresa";
+$i18n_array['precio_empresa'] = "Precio";
+
+
+?>
