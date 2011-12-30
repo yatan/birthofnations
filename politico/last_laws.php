5811729c796dd8141ca27419a69f273883d2d549
airumas@gmail.com <airumas@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2011-12-30 16:33:54 +0000

    Mostramos votaciones cerradas y arreglamos un pequeño bug si solo habia un PP en un pais.

--- politico/last_laws.php
+++ politico/last_laws.php
@@ -11,27 +11,34 @@ foreach ($sql as $votacion) {
 
     $si = sql("SELECT votos FROM candidatos_elecciones WHERE id_candidato =-1 AND id_votacion = " . $votacion['id_votacion']);
     $no = sql("SELECT votos FROM candidatos_elecciones WHERE id_candidato =-2 AND id_votacion = " . $votacion['id_votacion']);
-    echo proposal_text($votacion['tipo_votacion'],$votacion['id_votacion']);
+    echo proposal_text($votacion['tipo_votacion'], $votacion['id_votacion']);
     if (puedo_votar($objeto_usuario->id_usuario, $votacion['tipo_votacion'], $votacion['id_votacion'])) {
         echo "<br><a href=../politico/votar.php?idv=" . $votacion['id_votacion'] . "&vot=-1>Si: </a>" . $si;
         echo " <a href=../politico/votar.php?idv=" . $votacion['id_votacion'] . "&vot=-2>No: </a>" . $no;
     } else {//No puede votar pero que vea los resultados
-        echo "Si: " . $si;
-        echo "No: " . $no;
+        echo "<br>Si: " . $si;
+        echo " No: " . $no;
     }
 }
 ?><h3><? echo getString('closed_polls'); ?></h3><?
+$sql = sql2("SELECT id_votacion, tipo_votacion FROM votaciones WHERE solved = 1 AND is_cargo = 0 AND id_pais = " . $id_pais);
+foreach ($sql as $votacion) {
 
-function proposal_text($tipo,$id){
-    switch($tipo):
+    $si = sql("SELECT votos FROM candidatos_elecciones WHERE id_candidato =-1 AND id_votacion = " . $votacion['id_votacion']);
+    $no = sql("SELECT votos FROM candidatos_elecciones WHERE id_candidato =-2 AND id_votacion = " . $votacion['id_votacion']);
+    echo proposal_text($votacion['tipo_votacion'], $votacion['id_votacion']);
+
+    echo "<br>Si: " . $si;
+    echo " No: " . $no;
+}
+
+function proposal_text($tipo, $id) {
+    switch ($tipo):
         case 100:
-            $sql = sql("SELECT param1 FROM votaciones WHERE id_votacion = ".$id);
-            $txt = getString('1prop_100') ." ". $sql ." ". getString('2prop_100');
+            $sql = sql("SELECT param1 FROM votaciones WHERE id_votacion = " . $id);
+            $txt = getString('1prop_100') . " " . $sql . " " . getString('2prop_100');
             break;
     endswitch;
     return $txt;
 }
-
-
-
 ?>
