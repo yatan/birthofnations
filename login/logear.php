c11618ed762c4df4bf4e72b38ac884428115ffd6
Etchner@gmail.com <Etchner@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2012-01-01 19:51:52 +0000

    Bug #15  http://www.birthofnations.com/bugs/view.php?id=15

--- login/logear.php
+++ login/logear.php
@@ -8,29 +8,29 @@ select_lang();
  */
 
 if(!isset($_POST['nick']) || $_POST['nick']=="") //Si no introduce el nick
-die($login_form['login_nonick']);
+die(getString('login_nonick'));
 else
 $user = $_POST['nick'];
 
 if(!isset($_POST['pass']) || $_POST['pass']=="") //Si no introduce la pass
-die($login_form['login_nopass']);
+die(getString('login_nopass'));
 else
 $pass = md5($_POST['pass']);
 
 $consulta = sql("SELECT nick, id_usuario FROM usuarios WHERE nick='$user'");
 
 if($consulta==false)//El usuario no esta en la BD
-    die($login_form['login_nomatch']);
+    die(getString('login_nomatch'));
 else
     $consulta = checkban($consulta['id_usuario']);
     
 if($consulta == true ) //Si esta ban    
-    die($login_form['login_banned']);
+    die(getString('login_banned'));
 else
     $consulta = sql("SELECT id_usuario FROM usuarios WHERE nick='$user' AND password='$pass'");
 
 if($consulta==false)//No concuerdan user y pass
-    die($login_form['login_nomatch2']);
+    die(getString('login_nomatch2'));
 else
 {
     $_SESSION['id_usuario'] = $consulta;
