3b417d744268af06ce371b1d603440a89e931fa1
airumas@gmail.com <airumas@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2012-01-01 20:29:35 +0000

    Arreglando url en la subida anterior

--- politico/last_laws.php
+++ politico/last_laws.php
@@ -13,11 +13,11 @@ foreach ($sql as $votacion) {
     $no = sql("SELECT votos FROM candidatos_elecciones WHERE id_candidato =-2 AND id_votacion = " . $votacion['id_votacion']);
     echo proposal_text($votacion['tipo_votacion'], $votacion['id_votacion']);
     if (puedo_votar($objeto_usuario->id_usuario, $votacion['tipo_votacion'], $votacion['id_votacion'])) {
-        echo "<br><a href=../politico/votar.php?idv=" . $votacion['id_votacion'] . "&vot=-1>Si: </a>" . $si;
-        echo " <a href=../politico/votar.php?idv=" . $votacion['id_votacion'] . "&vot=-2>No: </a>" . $no;
+        echo "<br><a href=/politico/votar.php?idv=" . $votacion['id_votacion'] . "&vot=-1>Si: </a>" . $si;
+        echo " <a href=/politico/votar.php?idv=" . $votacion['id_votacion'] . "&vot=-2>No: </a>" . $no;
     } else {//No puede votar pero que vea los resultados
         echo "<br>Si: " . $si;
-        echo " No: " . $no;
+        echo " No: " . $no . "<br>";
     }
 }
 ?><h3><? echo getString('closed_polls'); ?></h3><?
