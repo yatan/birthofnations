050e2fa031c75571d84d9d7b3c78ef8ed80be335
yatan87@gmail.com <yatan@users.noreply.github.com>
2012-01-07 18:37:12 +0000

    *Pequeñas modificaciones en la mariconada de los dineros xD

--- status.php
+++ status.php
@@ -86,16 +86,16 @@ echo "<a style='padding-left: 3em; text-align:right;'>Dia: $dia</a>";
 
 <div id="barra_derecha" style="position: absolute; left:100%; top:50%; margin-left: -35px;">
     <img id="flecha" src="/images/side/flecha.gif"/>
-    <div id="monedas_barra" style="background-color: #1c94c4; position:absolute; display: none; top:50%; left:100%;margin-top: -60px; margin-left: -45px;">
-        <? include("usuarios/mi_perfil.php"); ?>
+    <div id="monedas_barra" style="z-index: 5; background-color: #1c94c4; position:absolute; display: none; top:50%; width:75px; left:100%;margin-top: 0px; margin-left: 0px;">
+        <? include("usuarios/dinero.php"); ?>
     </div>
 </div>
 
 <script>
 $("#barra_derecha").hover(
   function () {
-    $("#flecha").hide('fast');
-    $("#monedas_barra").show('fast');
+    $("#flecha").hide();
+    $("#monedas_barra").show();
   }, 
   function () {
     $("#flecha").show('fast');
--- usuarios/mi_perfil.php
+++ usuarios/mi_perfil.php
@@ -7,14 +7,7 @@
 
 echo "<h2>Mi economia</h2>";
 
-echo sql("SELECT Gold FROM money WHERE id_usuario='".$_SESSION['id_usuario']."'"). getString('Gold') ." <br/>";
-$sql = sql("SELECT * FROM money WHERE id_usuario='".$_SESSION['id_usuario']."'");
-
-arsort($sql);
-foreach ($sql as $moneda => $valor) {
-    if($moneda!="id_usuario" && $moneda!="Gold" && $valor>0)
-    echo $valor." ".$moneda."<br/>";
-}
+include("dinero.php");
 
 ?>
 
