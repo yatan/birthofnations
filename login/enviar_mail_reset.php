3ceabce182108178c958f14ba3d2dfec18ea9862
yatan87@gmail.com <yatan@users.noreply.github.com>
2012-01-11 00:49:52 +0000

    *Comprobacion de si existe el correo a recuperar

--- login/enviar_mail_reset.php
+++ login/enviar_mail_reset.php
@@ -15,6 +15,10 @@ elseif(isset($_POST['mail']) && $_POST['mail']=="")
 else
 {
     $correo = $_POST['mail'];
+    
+    if(sql("SELECT email FROM usuarios WHERE email='$correo'")==false)
+        die (getString('signup_mail_info'));
+    
     reset_mail($correo);
     echo "<p>".getString('login_reset_password_mail_sent')."</p><center><img src='/images/menu/si.png'/></center>";
     
