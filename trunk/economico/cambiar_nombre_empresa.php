4d8cfd4ff4c49364bd2ad5778bcadb4df844fd2a
tathtat@gmail.com <tathtat@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2012-01-21 15:29:33 +0000

    Ya no sale html en el nombre de la empresa.

--- trunk/economico/cambiar_nombre_empresa.php
+++ trunk/economico/cambiar_nombre_empresa.php
@@ -9,6 +9,7 @@
 if (isset($_POST['nuevo_nombre']) && $_POST['nuevo_nombre'] != "" && strlen($_POST['nuevo_nombre'])>0) 
 {
     
+    $_POST[nuevo_nombre]=htmlentities($_POST[nuevo_nombre], ENT_QUOTES | ENT_IGNORE, "UTF-8");   
     $sql = sql("SELECT id_empresa FROM empresas WHERE nombre_empresa = '" . $_POST['nuevo_nombre'] ."'");//Comprobamos que no este el nombre cogido.
     
     if($sql != false )
