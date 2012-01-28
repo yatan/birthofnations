a27750c73ac8f3ec782a87f5a126bda64abbfc52
yatan87@gmail.com <yatan@users.noreply.github.com>
2012-01-28 13:50:40 +0000

    +Uso del parser para los comentarios

--- periodico/nuevo_comentario.php
+++ periodico/nuevo_comentario.php
@@ -15,9 +15,21 @@ if(strlen($_POST['comentario'])<=0)
     die("Escribe algo");
 
 $comentario = htmlentities($_POST['comentario'], ENT_QUOTES | ENT_IGNORE, "UTF-8");
-$comentario = nl2br($comentario);
 
-sql("INSERT INTO comentarios_articulos (id_autor, comentario, id_articulo, fecha) VALUES ('{$_SESSION['id_usuario']}','$comentario','{$_POST['id_articulo']}','".time()."')");
+
+$c = curl_init();
+curl_setopt($c, CURLOPT_URL, 'http://birthofnations.com/js/markitup/sets/bbcode/parser.php');
+curl_setopt($c, CURLOPT_POST, true);
+curl_setopt($c, CURLOPT_POSTFIELDS, 'data='.$comentario);
+curl_setopt($c, CURLOPT_HEADER, 0);
+curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+
+$respuesta = curl_exec ($c);
+curl_close ($c); 
+
+
+//$comentario = nl2br($c);
+sql("INSERT INTO comentarios_articulos (id_autor, comentario, id_articulo, fecha) VALUES ('{$_SESSION['id_usuario']}','$respuesta','{$_POST['id_articulo']}','".time()."')");
 
 echo getString('comentario_publicado');
 
