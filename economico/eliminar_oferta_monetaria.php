b0553e28603a7f4d5f8aae5e19ecaaf2d30ad43a
yatan87@gmail.com <yatan@users.noreply.github.com>
2012-02-04 14:01:30 +0000

    *Arreglado bug en Mercado Monetario

--- economico/eliminar_oferta_monetaria.php
+++ economico/eliminar_oferta_monetaria.php
@@ -1,5 +1,6 @@
 <?php
 include_once($_SERVER['DOCUMENT_ROOT']."/include/funciones.php");
+include_once($_SERVER['DOCUMENT_ROOT'] . "/economico/moneda_local.php");
 /*
  * Code started by: yatan
  * Batamanta Team
@@ -7,12 +8,15 @@ include_once($_SERVER['DOCUMENT_ROOT']."/include/funciones.php");
 
 $id_oferta = $_POST['id_oferta'];
 $vendedor = sql("SELECT id_vendedor FROM mercado_monetario WHERE id_oferta='$id_oferta'");
-
+$moneda = sql("SELECT tipo_moneda_comprar FROM mercado_monetario WHERE id_oferta='$id_oferta'");
+$cantidad = sql("SELECT cantidad_moneda_comprar FROM mercado_monetario WHERE id_oferta='$id_oferta'");
 //Modulo de sobriedad (seguridad)
 if($vendedor != $_SESSION['id_usuario'])
     die("Vas a estar baneado ¬¬");
 
-
+$moneda = $moneda_local[$moneda];
 sql("DELETE FROM mercado_monetario WHERE id_oferta='$id_oferta'");
+sql("UPDATE money SET $moneda = $moneda + $cantidad WHERE id_usuario='$vendedor'");
+
 echo getString('offer_cancel_ok');
 ?>
