94058715ce206f5614d429d0c380d2a1a02d4bb4
tathtat@gmail.com <tathtat@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2012-02-05 18:22:55 +0000

    Añadido el gallego No links en los titulos del menu Logout funciona Link de Home con idioma

---
+++ i18n/gl.php
@@ -0,0 +1,392 @@
+<?php   
+		//REGISTRO
+
+$i18n_array['signup_title'] = "Formulario de Rexistro";
+$i18n_array['signup_username'] = "Nome de usuario ";
+$i18n_array['signup_username_info'] = "Caracteres de A-z; mínimo 5 caracteres ";
+$i18n_array['signup_pass'] = 'Contraseña';
+$i18n_array['signup_pass_info'] = "Mínimo 5 caracteres; máximo 12 caracteres; letras e símbolos";
+$i18n_array['signup_pass2'] = 'Repetir contraseña';
+$i18n_array['signup_pass_info2'] = "Debe ser igual á anterior ";
+$i18n_array['signup_referer'] = 'Referidor';
+$i18n_array['signup_mail'] = "email";
+$i18n_array['signup_mail_info'] = 'Escribe un email válido por favor';// No se usa??
+$i18n_array['signup_submit'] = "Formulario de Rexistro";// No se usa??
+$i18n_array['signup_mail_send_form'] = "Enviar formulario";
+$i18n_array['signup_mail_error'] = "Prodúxose un error ao conectar co servidor de mail.";
+
+                //GENERICAS
+
+$i18n_array['name_taken'] = "Ese nome esta ocupado.";
+$i18n_array['not_enough_gold']= ' Non tes oro suficiente.';
+$i18n_array['already_affiliated'] = 'Xa estas en un partido.';
+$i18n_array['smth_failed'] = "Algo ocurriu.";
+$i18n_array['new_name'] = "Novo nombre";
+$i18n_array['new_flag'] = "Nova bandera";
+$i18n_array['see_results'] = "Ver resultados";
+
+                //PALABRAS SUELTAS
+$i18n_array['leaders'] = "Lideres";
+$i18n_array['nick'] = 'Nick';
+$i18n_array['name'] = 'Nome';
+$i18n_array['exp'] = 'Experiencia: ';
+$i18n_array['population'] = 'Población: ';
+$i18n_array['position'] = 'Posicion';
+$i18n_array['votar'] = 'Votar';
+$i18n_array['vote'] = 'Vota';   //verb. Same as previous.
+$i18n_array['votes'] = 'Votos'; //Noun. Plural.
+$i18n_array['postulate'] = "Postulate";
+$i18n_array['unpostulate'] = "Despostulate";
+$i18n_array['money'] = 'Diñeiro';
+$i18n_array['currency'] = 'Moeda';
+$i18n_array['amount'] = 'Cantidade';
+$i18n_array['regions'] = 'Rexións';
+$i18n_array['affiliate'] = 'Afiliarse';
+$i18n_array['unaffiliate'] = 'Desafiliarse';
+$i18n_array['owner'] = 'Propietario';
+$i18n_array['si'] = 'Si';
+$i18n_array['no'] = 'Nom';
+$i18n_array['candidate'] = 'Candidato: ';
+$i18n_array['aceptar'] = 'Aceptar';
+$i18n_array['you_cant'] = 'O amado lider mirate con desaprobacion. Non cumples algun requisito.';
+$i18n_array['country'] = 'Pais';
+$i18n_array['region'] = 'Rexion';
+$i18n_array['id_destiny'] = 'Id_Destinatario';
+$i18n_array['Ofertas'] = 'Ofertas';
+$i18n_array['comprar'] = 'Comprar';
+$i18n_array['vender'] = 'Vender';
+$i18n_array['seller'] = 'Vendedor';
+$i18n_array['precio'] = 'Precio';
+$i18n_array['alternate_money'] = 'Alternar moedas';
+$i18n_array['search_offers'] = 'Buscar ofertas';
+$i18n_array['type'] = 'Tipo';
+
+
+ 		//LOGIN
+
+$i18n_array['login_texto'] = "Birth of Nations é un xogo online libre de estrategia política, económica e social. O desarrollo do xogo realizase de forma comunitaria e sus propios xogadores deciden cal é o rumbo que toma o proxecto.";
+$i18n_array['login_nonick'] = "Falta o usuario";
+$i18n_array['login_nopass'] = "Falta a contraseña";
+$i18n_array['login_nomatch'] = "A combinación de usuario/password non é a correcta [0x01]" ;
+$i18n_array['login_nomatch2'] = "A combinación de usuario/password non é a correcta [0x02]";
+$i18n_array['login_banned'] = "Fuches baneado"; 
+$i18n_array['login_recovery'] = "Recuperar password"; 
+
+		//FIRST LOGIN
+
+$i18n_array['first_login_title'] = "Benvido a Birth of Nations";
+$i18n_array['first_login_dialog'] = "Este é o teu primer login ao xogo. Selecciona o teu destino e convertete nun idiota";
+$i18n_array['first_login_travel'] = "Seleccionar pais destino:";
+
+//RECOVERY ACCOUNT
+
+
+$i18n_array['login_recovery_title'] = "Recuperar password";
+$i18n_array['login_recovery_email'] = "E-mail";
+$i18n_array['login_reset_password'] = "Resetear password";
+$i18n_array['login_reset_password_no_mail'] = "Sen email non se podrá recuperar a cuenta";
+$i18n_array['login_reset_password_mail_sent'] = "Enviouse o email coas instruccións";
+
+
+
+$i18n_array['code_error'] = "Error no codigo";
+$i18n_array['referer_title'] = "Alguén invitouche a Birth of Nations";
+$i18n_array['referer_mail1'] = "Benvido<br/>Recibes este correo porque: ";
+$i18n_array['referer_mail2'] = " invitouche ao xogo.<br/>Para aceptar a invitacion fai click ao seguinte enlace, en caso contrario ignora o correo.<br/>";
+$i18n_array['mail_bienvenida1'] = "<p><img src='http://birthofnations.com/images/titulo.png' alt='logo' width='498' height='99' align='middle' />
+                            </p>
+                            <p>&nbsp;</p>
+                            <p>Benvido a Birth of Nations os teus datos de registro son:</p>
+                            <p>Usuario: ";
+$i18n_array['mail_bienvenida2'] = "</p>
+    <p>Automaticamente se ha creado la cuenta en el foro y la gestion de bugs con los mismos datos que el juego.</p>
+    <p><u><strong>Foro:</strong></u></p>
+    <p> <a href='http://birthofnations.com/forum'>http://birthofnations.com/forum</a></p>
+    <p><strong><u>Reporte de bugs:</u></strong></p>
+    <p> <a href='http://birthofnations.com/bugs/'>http://birthofnations.com/bugs/</a></p>
+    <p><strong><u>Tickets de soporte:</u></strong></p>
+    <p> <a href='http://birthofnations.com/support/'>http://birthofnations.com/support/</a></p>
+    <p>&nbsp;</p>
+    <p>Disfruta del juego</p>
+    <p> <a href='http://birthofnations.com/'>http://birthofnations.com/</a></p>
+    <p>&nbsp;</p>";
+$i18n_array['mail_bienvenida_title'] = "Benvido a Birth of Nations";
+
+//Economico
+$i18n_array['Gold'] = 'Gold';
+$i18n_array['Poner_ofertas_trabajo'] = "Por ofertas de traballo";
+$i18n_array['empresa_tipo1'] = "Pan";
+$i18n_array['empresa_tipo2'] = "Trigo";
+$i18n_array['empresa_tipo3'] = "Oil";
+$i18n_array['empresa_tipo4'] = "Transporte";
+$i18n_array['no_ofertas_trabajo'] = "Non hay ofertas de traballo";
+
+//Estos 3 deberian desaparecer
+$i18n_array['item1'] = "Sugus";
+$i18n_array['item2'] = "Azucar";
+$i18n_array['item3'] = "Armas";
+$i18n_array['item4'] = "Plumas";
+$i18n_array['item10'] = "Cuchillo";
+
+//Usuarios
+$i18n_array['cant_travel'] = 'Non cumples os requisitos necesarios para viaxar, revisa a tua existencia, mortal.';
+$i18n_array['cant_work'] = 'Necesitas 10 puntos de salud para traballar.';
+$i18n_array['no_train_health'] = 'Necesitas 10 puntos de salud para entrenar.';
+$i18n_array['no_mensajes'] = 'Non tés mensajes';
+$i18n_array['no_friends'] = "Sen amigos";
+$i18n_array['no_cantidad'] = "Por favor introduce cantidad correcta";
+$i18n_array['operacion_ok'] = "A operación realizouse correctamente";
+$i18n_array['me_falta_dinero'] = "Non dispoño de tanto dinero";
+$i18n_array['add_friend_head'] = "Peticion de amistade";
+$i18n_array['add_friend'] = "quere ser o teu amigo. �Aceptas?";
+$i18n_array['stat_a'] = "Hambrento";
+$i18n_array['stat_b'] = "Necesita sexo";
+$i18n_array['stat_c'] = "Sobredosis de azucar";
+$i18n_array['stat_v'] = "Viaxando";
+$i18n_array['stat_S'] = "Suicida";
+$i18n_array['no_stat'] = "Ben";
+
+//Pagina mantenimiento
+$i18n_array['mantenimiento_1'] = "Modo mantemento";
+$i18n_array['mantenimiento_2'] = "O xogo encontrase en mantemento.";
+$i18n_array['mantenimiento_3'] = "Para visitar o foro use o seguinte link:";
+$i18n_array['mantenimiento_4'] = "Acceder ao foro";
+
+// Mediatico
+$i18n_array['periodico_publicado'] = 'Articulo publicado';
+$i18n_array['periodico_caracter_blanco'] = 'Deixaches algun caracter en blanco';
+$i18n_array['periodico_articulo_noexiste'] = 'Este articulo non existe...';
+$i18n_array['periodico_no_recientes'] = 'Non hai articulos nos ultimos 5 dias.';
+$i18n_array['periodico_ultimos'] = 'Ultimos artículos';
+$i18n_array['periodico_top'] = 'Top artículos';
+$i18n_array['has_votado_positivamente'] = 'Votado positivamente';
+$i18n_array['has_votado_negativamente'] = 'Votado negativamente';
+$i18n_array['has_borrado_voto'] = 'Eliminaches o teu voto';
+$i18n_array['periodico_ya_has_votado'] = 'Xa votaches este articulo';
+$i18n_array['periodico_articulo_no_existe'] = 'Este articulo non existe!';
+$i18n_array['publicar_comentario'] = 'Publicar comentario';
+$i18n_array['comentario_publicado'] = 'Comentario añadido correctamente';
+
+
+//Politico
+$i18n_array['already_voted'] = 'Xa votaches';
+$i18n_array['no_regions'] = "Este pais non posue ningunha region actualmente";
+$i18n_array['open_polls'] = "Votacions abiertas";
+$i18n_array['closed_polls'] = "Votacions cerradas";
+$i18n_array["been_unpostulated"] = "Fuches despostulado.";
+$i18n_array["no_postulated"] = "Non estabas postulado.";
+$i18n_array['new_country'] = "Novo pais";
+$i18n_array['country_name'] = "Nome do pais:";
+$i18n_array['currency_name'] = "Nome da moneda:";
+$i18n_array['new_party'] = "Novo partido";
+$i18n_array['party_name'] = "Nome do partido";
+$i18n_array['min_days_to_vote'] = "Antiguedad necesaria para votar";
+$i18n_array['elections_frequency'] = "Frecuencia das elecciones";
+$i18n_array['open_elections'] = "Eleccións abiertas";
+$i18n_array['next_elections'] = "Proximas elecciones o dia: ";
+$i18n_array['vote_day'] = 'Dia de votar';
+$i18n_array['available_laws'] = 'Leis disponibles: ';
+$i18n_array['next_elections'] = 'Proximas eleccións: ';
+$i18n_array['member_list'] = 'Lista de membros';
+$i18n_array['gov_type_1'] = 'Anarquía';
+$i18n_array['gov_type_2'] = 'Consello de Guerreros';
+$i18n_array['gov_type_3'] = 'Consello de Sabios';
+
+
+//Menu
+
+$i18n_array['economico'] = "Economico";
+$i18n_array['militar'] = "Militar";
+$i18n_array['mercados'] = "Mercados";
+$i18n_array['pais'] = "Pais";
+$i18n_array['soporte'] = "Soporte";
+$i18n_array['trabajar'] = "Trabajar";
+$i18n_array['mis_empresas'] = "Miñas Empresas";
+$i18n_array['perfil'] = "Meu Perfil";
+$i18n_array['entrenamiento'] = "Entrenamento";
+$i18n_array['guerras'] = "Guerras";
+$i18n_array['mercado_productos'] = "Mercado de productos";
+$i18n_array['mercado_laboral'] = "Mercado laboral";
+$i18n_array['mercado_economico'] = "Mercado monetario";
+$i18n_array['mercado_empresas'] = "Mercado de empresas";
+$i18n_array['mi_pais'] = "Meu País";
+$i18n_array['partidos_politicos'] = "Partidos politicos";
+$i18n_array['ranking'] = "Clasificación";
+$i18n_array['foro'] = "Foro";
+$i18n_array['tickets'] = "Tickets de soporte";
+$i18n_array['bugs'] = "Reporte de bugs";
+$i18n_array['wiki'] = "Wiki";
+$i18n_array['laws'] = "Leis";
+//Items
+
+$i18n_array['sugus'] = "Sugus";
+$i18n_array['azucar'] = "Azucar";
+$i18n_array['armas'] = "Armas";
+$i18n_array['hierro'] = "Ferro";
+//Leyes
+
+$i18n_array['1prop_2'] = 'Cambiar tipo de gobierno'; 
+$i18n_array['law_100'] = 'Cambiar nombre al pais';
+$i18n_array['1prop_100'] = 'Llamar';
+$i18n_array['2prop_100'] = 'al pais';
+$i18n_array['law_101'] = 'Añadir cargo';
+$i18n_array['law_102'] = 'Eliminar cargo';
+$i18n_array['law_103'] = 'Dar cargo';
+$i18n_array['law_104'] = 'Quitar cargo';
+$i18n_array['law_105'] = 'Cambiar bandeira';
+$i18n_array['1prop_105'] = 'Nova bandera do pais: ';
+$i18n_array['law_106'] = 'Cambiar nome da moeda';
+$i18n_array['1prop_106'] = 'Novo nome da moeda do pais';
+$i18n_array['law_200'] = 'Emitir mneda';
+$i18n_array['1prop_200'] = 'Emitir ';
+$i18n_array['law_201'] = 'Doar moeda';
+$i18n_array['1prop_201'] = 'Doar ';
+$i18n_array['2prop_201'] = ' a ';
+//Cargos
+
+$i18n_array['cargo_2_1'] = "Sabio";
+$i18n_array['cargo_3_1'] = "Guerreiro";
+
+//Rangos Militares
+
+$i18n_array['rango_0'] = 'Soldado';
+$i18n_array['rango_1'] = 'Tirillas';
+//PORTADA
+
+$i18n_array['periodicos_login'] = 'Xornales';
+$i18n_array['guerras_login'] = 'Últimas guerras';
+
+//ERRORES
+
+$i18n_array['generic_error'] = "Error";
+$i18n_array['conection_fail_1'] = 'Hai un fallo nos servidores. Intentao mais tarde';
+//PIE
+
+$i18n_array['foot_status_begin'] = 'Hai: ';
+$i18n_array['foot_status_mysql_user'] = "usuarios online";
+$i18n_array['foot_status_mysql_status'] = "mysql status: ";
+
+
+//COMPANIES
+$i18n_array['company_error_left_work'] = "Necesitas sair do teu traballo";
+$i18n_array['company_item_use'] = "Usar: ";
+$i18n_array['company'] = "Empresa:";
+$i18n_array['company_error_not_valid'] = "Empresa non valida";
+$i18n_array['company_ups_something_is_wrong'] = "Algo falla";
+$i18n_array['company_inventory'] = "Inventario da empresa";
+$i18n_array['use_item'] = "Usar item";
+$i18n_array['sell_stock'] = "Vender stock";
+$i18n_array['sales'] = "Ventas";
+$i18n_array['button_change_salary'] = "Cambiar salario";
+$i18n_array['company_money'] = "Diñeiro";
+$i18n_array['cantidad'] = "Cantidade";
+$i18n_array['coin'] = "Moeda";
+$i18n_array['ingresar'] = "Ingresar";
+$i18n_array['retirar'] = "Retirar";
+$i18n_array['vender_empresa'] = "Vender empresa";
+$i18n_array['precio_empresa'] = "Precio";
+$i18n_array['salary'] = "salario";
+$i18n_array['economy'] = "Economía";
+$i18n_array['cantidad'] = "Cantidade";
+$i18n_array['name_changed'] = "Nome cambiado";
+$i18n_array['not_enough_data'] = "Faltan datos";
+$i18n_array['increase_salary'] = "Pon salarios maiores a 0";
+$i18n_array['salary_succesfully_changed'] = "Salario modificado correctamente";
+$i18n_array['buy_succes'] = "Compra realizada correctamente";
+$i18n_array['not_enough_money'] = "Non tienes suficiente diñeiro";
+$i18n_array['not_enough_sugus'] = "Non tienes sugus suficientes";
+$i18n_array['created_succes'] = "Empresa creada correctamente";
+$i18n_array['company_the_name_is_taken'] = "O nombre está en uso";
+$i18n_array['company_work'] = "Traballar";
+$i18n_array['company_owner'] = "Propietario";
+$i18n_array['company_type'] = "Tipo";
+$i18n_array['company_no_tienes'] = "Non tés";
+$i18n_array['company_my_companies'] = "Miñas empresas";
+$i18n_array['company_list_name'] = "Nome";
+$i18n_array['company_create_bussiness'] = "Crear empresa";
+$i18n_array['company_create_send'] = "Enviar";
+$i18n_array['golds'] = "golds";
+$i18n_array['company_object_selection'] = "Selección de obxeto";
+$i18n_array['company_object_market'] = "Mercado de obxetos de: ";
+$i18n_array['company'] = "Empresa";
+$i18n_array['company_accept'] = "Aceptar";
+$i18n_array['company_oferta_anyadida'] = "Oferta añadida correctamente";
+$i18n_array['company_not_enough_salary'] = "Falta diñeiro na empresa para pagarte";
+$i18n_array['company_you_have_worked'] = "Xa traballaches hoxe";
+$i18n_array['company_not_enough_raw'] = "Faltan as materias necesarias para producir";
+$i18n_array['company_exp'] = "Gañado 1 punto de experiencia";
+$i18n_array['company_you_have_produced'] = "Produciches ";
+$i18n_array['company_come_again_tomorrow'] = "Volve mañá";
+$i18n_array['no_job_offers'] = "Non hai ofertas de traballo.";
+$i18n_array['no_selling_companies'] = "Non hai empresas á venta.";
+$i18n_array['not_enough_stock'] = "Non tés suficiente stock para vender";
+$i18n_array['sell_for_zero'] = "O precio de venta non pode ser negativo";
+
+// VIAJES
+
+$i18n_array['moved_ok'] = "Viaxaches correctamente";
+$i18n_array['you_are_in_a_country'] = "¡¡X estás nun país!!";
+$i18n_array['choose_a_region'] = "Selecciona unha rexión";
+
+
+// VALIDATION_SIGNUP
+
+$i18n_array['no_user'] = "Falta usuario";
+$i18n_array['no_password'] = "Falta password";
+$i18n_array['no_re_password'] = "Falta validación de password";
+$i18n_array['no_mail'] = "Falta e-mail";
+$i18n_array['no_referer'] = "Falta referer";
+$i18n_array['error_re_password'] = "Error ao validad password";
+$i18n_array['user_again'] = "Xa existe un usuario con ese nick";
+$i18n_array['mail_again'] = "Xa existe un e-mail igual registrado";
+$i18n_array['ok'] = "ok";
+
+//MILITAR
+$i18n_array['military_strength'] = 'Forza: ';
+$i18n_array['military_range'] = 'Rango: ';
+$i18n_array['military_points'] = 'Puntos de combate: ';
+$i18n_array['military_you_have_trained'] = 'Xa entrenaches, volve mañá: ';
+$i18n_array['military_train'] = 'Entrenar ';
+$i18n_array['military_accept'] = 'Aceptar ';
+$i18n_array['more_strenght'] = 'A tua forza aumentou';
+$i18n_array['more_exp'] = 'Os teus puntos de experiencia aumentaron ';
+
+
+//QUESTs
+
+$i18n_array['quest_valle'] = 'En busca dos lobos enfadados ';
+$i18n_array['quest_valle_text'] = 'Ve ao bosque Willon e mata 10 lobos. ';
+$i18n_array['quest_valle_recompensa'] = '12 de Experiencia e 2 pociones';
+
+
+//PIE 
+
+$i18n_array['online_users'] = "usuarios online Estado mysql: ";
+$i18n_array['copyright'] = "Copyright 2011 Batamanta Team ";
+
+
+//MENU.PHP
+
+$i18n_array['work'] = "Traballar";
+$i18n_array['logout'] = "Logout";
+
+
+// INDEX.PHP
+
+$i18n_array['last_wars'] = "Últimas guerras";
+
+
+//FORM_REFERER.PHP
+
+$i18n_array['mail_server_error'] = "O servidor de correo non esta dispoñible actualmente";
+$i18n_array['enviar_invitacion'] = "Enviar invitación";
+
+//mercado_economico.php
+$i18n_array['post_offer'] = "Publicar oferta";
+$i18n_array['offer'] = "Ofrecer";
+$i18n_array['monedas_iguales'] = "O tipo de moeda non poode ser o mesmo";
+$i18n_array['offer_cancel'] = "Cancelar oferta";
+$i18n_array['offer_cancel_ok'] = "Oferta cancelada correctamente";
+$i18n_array['offer_lower_zero'] = "A cantidade introducida é menor que 0.01";
+
+?>
