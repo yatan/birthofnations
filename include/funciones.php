eae3b0abfaa3e0cbd40a640884d521f7e1701e25
Airumas@gmail.com <Airumas@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2012-02-05 16:11:00 +0000

    *Otro pequeño bug

--- include/funciones.php
+++ include/funciones.php
@@ -846,7 +846,7 @@ function apply_law($vot) {
                     break;
                 case 3://Consejo de guerreros
                     $cargos = 100 * $votacion['id_votacion'];
-                    sql("INSERT INTO country_leaders(id_cargo,nombre,votacion,laws) VALUES (" . $down . "," . getString('cargo_3_1') . ",'A','100-V.R+" . $down . ",105-V.R+" . $down . "')");
+                    sql("INSERT INTO country_leaders(id_cargo,nombre,votacion,laws) VALUES (" . $down . ",'" . getString('cargo_3_1') . "','A','100-V.R+" . $down . ",105-V.R+" . $down . "')");
                     $gente = sql("SELECT id_usuario FROM usuarios WHERE id_nacionalidad = " . $votacion['id_pais'] . " ORDER BY fuerza DESC LIMIT 9");
                     foreach ($gente as $id) {
                         add_leader($cargos, $id['id_usuario']);
