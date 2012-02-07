6888862991b3d7d6f049d679eac8ce9c3688d067
yatan87@gmail.com <yatan87@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2012-02-07 14:36:03 +0000

    +Seguridad al despedir v0.2

--- trunk/economico/despedir.php
+++ trunk/economico/despedir.php
@@ -11,8 +11,14 @@ if (isset($_GET['id_worker']) && $_GET['id_worker'] != "" && strlen($_GET['id_wo
 
 $data = sql("SELECT nombre_empresa, id_propietario FROM empresas WHERE id_empresa = (SELECT id_empresa FROM usuarios WHERE id_usuario  = ".$_GET['id_worker']." )");
 
+if($data==NULL)
+{
+    incidencia($id_usuario, 1, "Intento de despedir a alguien");
+    die("ERROR notificado a un administrador");
+}    
+
 $propietario = $data['id_propietario'];
-$empresa = $data['empresa'];
+$empresa = $data['nombre_empresa'];
 
 if($id_usuario != $propietario)
 {
