b1ed4ad07a4a944f77532acac891094f29bede9a
Airumas@gmail.com <Airumas@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2012-02-07 19:05:51 +0000

    *Decimales al vender objetos *Comprobamos que al poner ofertas de trabajo y vender objetos la cantidad sea mayor a 1 y se redondea hacia abajo

--- economico/poner_oferta.php
+++ economico/poner_oferta.php
@@ -7,6 +7,8 @@
 if (isset($_POST['salario']) && $_POST['salario'] != "" && is_numeric($_POST['salario']) && isset($_POST['cantidad']) && $_POST['cantidad'] != "" && is_numeric($_POST['cantidad']))
 {
     if($_POST['salario'] >= 0){
+        $_POST['cantidad'] = floor($_POST['cantidad']);//Por si las moscas 
+        $_POST['salario'] = round($_POST['salario'], 2, PHP_ROUND_HALF_DOWN);//Por si las moscas 
     sql("INSERT INTO mercado_trabajo(id_pais, id_empresa, id_jefe, salario, cantidad) VALUES ('".$_POST['id_pais']."','".$_POST['id_empresa']."', '". $_SESSION['id_usuario'] ."', '".$_POST['salario']."','".$_POST['cantidad']."') ");
 
     echo getString('company_oferta_anyadida'); 
