32e6165b0f685ce470505666ca105ba8cc49e2a2
Airumas@gmail.com <Airumas@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2012-03-04 18:17:05 +0000

    *Ahora si alguien del govierno upgradea una tecnologia el dinero sale del pais ^SI no, lo paga el user.

--- trunk/politico/tecnologia.php
+++ trunk/politico/tecnologia.php
@@ -12,30 +12,39 @@ include_once($_SERVER['DOCUMENT_ROOT'] . "/politico/objeto_pais.php");
 
 $tech = $_GET['tech'];
 $pais = $_GET['pais'];
+$id_usuario = $_SESSION['id_usuario'];
 
-//Comprobar Gold del pais
+$gov = check_gov($id_usuario, $pais);
 
-$gold_pais = sql("SELECT Gold FROM money_pais WHERE idcountry = ".$pais);
+//Comprobar Gold del que pague
+if ($gov == true) {//Paga el pais
+    $gold_pais = sql("SELECT Gold FROM money_pais WHERE idcountry = " . $pais);
+} else {//Paga el user
+    $gold_pais = sql("SELECT Gold FROM money WHERE id_usuario = " . $id_usuario);
+}
 
 //Sacar precio de la renovacion
 
-$precio = sql("SELECT precio".$tech . " FROM country_tech WHERE id_country = ".$pais);
+$precio = sql("SELECT precio" . $tech . " FROM country_tech WHERE id_country = " . $pais);
 
 //Comparar
 
-if($gold_pais >= $precio){//Hay pasta
-    //Quitar dinero al pais
-    sql("UPDATE money_pais SET Gold = Gold - ".$precio." WHERE idcountry = ".$pais);
-    //Añadir dias
-    $tiempo = sql("SELECT tech".$tech." FROM country_tech WHERE id_country = ".$pais);
-    $time = time_tech($tech);
-    if($tiempo >=0){
-    sql("UPDATE country_tech SET tech".$tech." = tech".$tech." + ".$time." WHERE id_country = ".$pais);
-    }else{
-        sql("UPDATE country_tech SET tech".$tech." = ".$time." WHERE id_country = ".$pais);
+if ($gold_pais >= $precio) {//Hay pasta
+    //Quitar dinero
+    if ($gov == true) {//Paga el pais
+        sql("UPDATE money_pais SET Gold = Gold - " . $precio . " WHERE idcountry = " . $pais);
+    } else {//Paga el user
+        sql("UPDATE money SET Gold = Gold - " . $precio . " WHERE id_usuario = " . $id_usuario);
     }
-}else{
+    //Añadir dias
+    $tiempo = sql("SELECT tech" . $tech . " FROM country_tech WHERE id_country = " . $pais);
+    $time = time_tech($tech);
+    if ($tiempo >= 0) {
+        sql("UPDATE country_tech SET tech" . $tech . " = tech" . $tech . " + " . $time . " WHERE id_country = " . $pais);
+    } else {
+        sql("UPDATE country_tech SET tech" . $tech . " = " . $time . " WHERE id_country = " . $pais);
+    }
+} else {
     //No hay pasta
 }
-
 ?>
