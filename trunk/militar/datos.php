72e938e50dd24908fde4e8a4cf94cccc6d6592da
yatan87@gmail.com <yatan87@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2012-03-12 11:35:57 +0000

    *Arreglado los battle heroes

--- trunk/militar/datos.php
+++ trunk/militar/datos.php
@@ -17,11 +17,11 @@ $muro = ( $total_atacantes / ($total_atacantes + $total_defensores )) * 100;
 $muro = rfloor($muro, 0);
 $muro = str_replace(".", "", $muro);
 
-$top_a = sql("SELECT id_usuario, SUM(hit) FROM log_hits WHERE id_guerra = 1 AND lado='a' GROUP BY id_usuario DESC LIMIT 1");
+$top_a = sql("SELECT id_usuario, SUM(hit) FROM log_hits WHERE id_guerra = 1 AND lado='a' GROUP BY id_usuario ORDER BY SUM(hit) DESC LIMIT 1");
 $top_a_nick = id2nick($top_a['id_usuario']);
 $top_a_dano = $top_a['SUM(hit)'];
 
-$top_d = sql("SELECT id_usuario, SUM(hit) FROM log_hits WHERE id_guerra = 1 AND lado='d' GROUP BY id_usuario DESC LIMIT 1");
+$top_d = sql("SELECT id_usuario, SUM(hit) FROM log_hits WHERE id_guerra = 1 AND lado='d' GROUP BY id_usuario ORDER BY SUM(hit) DESC LIMIT 1");
 $top_d_nick = id2nick($top_d['id_usuario']);
 $top_d_dano = $top_d['SUM(hit)'];
 
