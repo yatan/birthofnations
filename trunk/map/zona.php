5444fbbc54aa0ad592733ffcc8e79b28ff30600c
airumas@gmail.com <airumas@gmail.com@5e4f3f4a-324e-2c20-6831-c868057a363a>
2013-06-23 17:15:18 +0000

    + Se muestra adecuadamente el dueño de cada cuadradito de cada zona

--- trunk/map/zona.php
+++ trunk/map/zona.php
@@ -41,8 +41,10 @@ var quien;
 })	
 	
 </script>
-
-    <center><h1>Mapa Zona <? echo sql("SELECT name FROM zona_data WHERE id_zona='$id_zona'"); ?></h1></center>
+<?
+$zona = sql("SELECT name,id_region FROM zona_data WHERE id_zona='$id_zona'");
+?>
+    <center><h1><? echo $zona['name'] ." - ". region2name($zona['id_region']) ?></h1></center>
 <div class="menu_izq">
   <p>Menu</p>
   <p>Precio:</p>
@@ -68,18 +70,28 @@ for($y=0;$y<11;$y++)
         {
         $tipo = $cuadros[$n]['tipo'];
         $id_prop = $cuadros[$n]['id_prop'];
+        $tipo_prop = $cuadros[$n]['tipo_prop'];
         $n++;
         
-        if($tipo==1)
-            echo "<img src='/images/map/hierba.png'/>";
+        //Sacar el nombre del propietario
+        if($tipo_prop == 1)
+        {
+            $nombre_prop = country_name($id_prop);
+        }elseif($tipo_prop == 2)
+        {
+            $nombre_prop = id2nick($id_prop);
+        }
+        
+        if($tipo==0)
+            echo "<a href='/es/ayuntamiento'><img class='elemento1' propietario='".$nombre_prop."' src='/images/map/ayuntamiento.jpg'/></a>";        
+        elseif($tipo==1)
+            echo "<img class='elemento1' src='/images/map/hierba.png' propietario='".$nombre_prop."'/>";
         elseif($tipo==2)
-            echo "<img class='elemento1' propietario='".id2nick($id_prop)."' src='/images/map/house.png'/>";
+            echo "<img class='elemento1' propietario='".$nombre_prop."' src='/images/map/house.png'/>";
         elseif($tipo==3)
-            echo "<img class='elemento1' propietario='".id2nick($id_prop)."' src='/images/map/car.png'/>";
-        elseif($tipo==0)
-            echo "<a href='/es/ayuntamiento'><img class='elemento1' propietario='Pais' src='/images/map/ayuntamiento.jpg'/></a>";        
+            echo "<img class='elemento1' propietario='".$nombre_prop."' src='/images/map/car.png'/>";
         elseif($tipo==4)
-            echo "<img class='elemento1' propietario='En venta' src='/images/map/venta.png'/>";         
+            echo "<img class='elemento1' propietario='".$nombre_prop."' src='/images/map/venta.png'/>";         
         
         }
        }
